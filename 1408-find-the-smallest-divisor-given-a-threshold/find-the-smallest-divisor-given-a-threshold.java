8a0c2a44bd1b5b74b128b006b450ad06b86a588c
Manav Desai <dmanavpianist@gmail.com>
2025-05-23 16:58:47 +0530

    Time: 28 ms (53.80%) | Memory: 48.4 MB (27.30%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -2,58 +2,39 @@ class Solution {
 
     int calculate_sum(int[] nums, int x) {
         int sum = 0;
-
         for (int num : nums) {
-            sum = sum + (int) Math.ceil((double) num / x);
+            sum += (int) Math.ceil((double) num / x);
         }
         return sum;
     }
 
     int find_max(int[] nums) {
         int max = Integer.MIN_VALUE;
-
         for (int num : nums) {
             if (num > max) {
                 max = num;
             }
         }
-
         return max;
     }
 
     int binary_search(int[] nums, int target) {
-
         int low = 1;
         int high = find_max(nums);
-        int ans = 0;
-        int mid = 0; 
-        int max_sum=Integer.MIN_VALUE;
+        int ans = high; // assume worst case (largest divisor)
+
         while (low <= high) {
-
-            mid = (low + high) / 2;
-
+            int mid = (low + high) / 2;
             int sum = calculate_sum(nums, mid);
 
             if (sum <= target) {
-                
-                if(sum>=max_sum){
-                    if(sum==max_sum){
-                        if(mid<ans){
-                            ans = mid;
-                        }
-                    }else{
-                        max_sum=sum;
-                        ans = mid;
-                    }                    
-                }
-                
-                high = mid - 1;
-                
+                ans = mid;         // valid divisor
+                high = mid - 1;    // try to find smaller one
             } else {
-                low = mid + 1;
+                low = mid + 1;     // need larger divisor
             }
-
         }
+
         return ans;
     }
 
