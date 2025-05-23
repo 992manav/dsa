d854bd8d2d5827abdb72ea0420991bb8ae6ebb4b
Manav Desai <dmanavpianist@gmail.com>
2025-05-23 16:59:15 +0530

    Time: 28 ms (53.80%) | Memory: 48.7 MB (10.46%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -2,39 +2,58 @@ class Solution {
 
     int calculate_sum(int[] nums, int x) {
         int sum = 0;
+
         for (int num : nums) {
-            sum += (int) Math.ceil((double) num / x);
+            sum = sum + (int) Math.ceil((double) num / x);
         }
         return sum;
     }
 
     int find_max(int[] nums) {
         int max = Integer.MIN_VALUE;
+
         for (int num : nums) {
             if (num > max) {
                 max = num;
             }
         }
+
         return max;
     }
 
     int binary_search(int[] nums, int target) {
+
         int low = 1;
         int high = find_max(nums);
-        int ans = high; // assume worst case (largest divisor)
-
+        int ans = 0;
+        int mid = 0; 
+        int max_sum=Integer.MIN_VALUE;
         while (low <= high) {
-            int mid = (low + high) / 2;
+
+            mid = (low + high) / 2;
+
             int sum = calculate_sum(nums, mid);
 
             if (sum <= target) {
-                ans = mid;         // valid divisor
-                high = mid - 1;    // try to find smaller one
+                
+                if(sum>=max_sum){
+                    if(sum==max_sum){
+                        if(mid<ans){
+                            ans = mid;
+                        }
+                    }else{
+                        max_sum=sum;
+                        ans = mid;
+                    }                    
+                }
+                
+                high = mid - 1;
+                
             } else {
-                low = mid + 1;     // need larger divisor
+                low = mid + 1;
             }
-        }
 
+        }
         return ans;
     }
 
