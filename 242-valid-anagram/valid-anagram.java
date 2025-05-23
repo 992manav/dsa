79b816f6336f582d8097fafe20a24e5220a4117f
Manav Desai <dmanavpianist@gmail.com>
2025-05-23 23:27:03 +0530

    Time: 17 ms (19.74%) | Memory: 44.8 MB (26.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,23 +6,22 @@ class Solution {
             return false;
         }
 
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-    
-        
-       for(int i=0;i<t.length();i++){
-            char c=t.charAt(i);
-            if(map.containsKey(c)){
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            if (map.containsKey(c)) {
                 map.put(c, map.get(c) - 1);
-                 if (map.get(c) < 0) {
+                if (map.get(c) < 0) {
                     return false;
                 }
-            }else{
-               return false;
+            } else {
+                return false;
             }
-       }
-       return true;
+        }
+        return true;
     }
 }
