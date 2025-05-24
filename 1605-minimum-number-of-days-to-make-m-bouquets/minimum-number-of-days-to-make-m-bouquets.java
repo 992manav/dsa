dfdf467a58513cf18a0bfc8a756121498918fb91
Manav Desai <dmanavpianist@gmail.com>
2025-05-24 23:35:21 +0530

    Time: 18 ms (72.02%) | Memory: 58.4 MB (48.96%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,9 +1,9 @@
 class Solution {
 
     boolean check(int[] arr,int mid,int m,int k){
-      
         int count=0;
         int flag=0;
+
         for(int i=0;i<arr.length;i++){
             if(arr[i]<=mid){
                flag++;
@@ -15,42 +15,33 @@ class Solution {
                 flag=0;
             }
         }
-        if(count<m){
-            return false;
-        }
-
-        return true;
+        return count >= m;
     }
 
     int binary_search(int[] arr,int m,int k){
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
 
-                int[] brr = arr.clone();
+        for (int val : arr) {
+            low = Math.min(low, val);
+            high = Math.max(high, val);
+        }
 
-                Arrays.sort(brr);
-
-                int low=0;
-                int high=brr.length-1;
-                int ans=high;
-                while(low<=high){
-
-                    int mid = low + (high - low) / 2;
-                    if(check(arr,brr[mid],m,k)){
-                        ans=mid;
-                        high=mid-1;
-                    }else {
-                    low=mid+1;
-                    }
-                }
-            return brr[ans];
-
+        int ans = high;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(check(arr, mid, m, k)){
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
-    public int minDays(int[] arr, int m, int k) {
-        if ((long)m * (long)k > arr.length) {
-            return -1;
-        }
 
-        else{
-            return binary_search(arr,m,k);
-        }
+    public int minDays(int[] arr, int m, int k) {
+        if ((long)m * (long)k > arr.length) return -1;
+        return binary_search(arr, m, k);
     }
 }
