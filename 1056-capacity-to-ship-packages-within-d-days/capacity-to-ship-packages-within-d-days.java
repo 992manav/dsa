cf0f3f3eacb30de727c0c216f8c27b44577babd3
Manav Desai <dmanavpianist@gmail.com>
2025-05-25 12:35:14 +0530

    Time: 11 ms (75.74%) | Memory: 47.6 MB (28.04%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,66 +1,63 @@
 class Solution {
 
-    int[] find_max(int[] arr){
-        
-        int max=Integer.MIN_VALUE;
-        int sum=0;
-        for(int ele : arr){
-            if(ele>max){
-                max=ele;
+   
+
+    // Find the maximum value and total sum of the array
+    public static int[] find_max(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int ele : arr) {
+            if (ele > max) {
+                max = ele; // update max
             }
-            sum=sum+ele;
+            sum = sum + ele; // calculate total sum
         }
-        // int[] a=new int[2];
-        // a[0]=max;
-        // a[1]=sum;
-        // return a;
-
-        return new int[]{max, sum};
+        return new int[]{max, sum}; // return both as an array
     }
 
+    // Check if it's possible to ship within given days using max 'weight'
+    public static boolean check(int[] arr, int weight, int days) {
+        int sum = 0;
+        int day = 1; // start from day 1
 
-    boolean check(int[] arr, int weight, int days) {
-    int sum = 0;
-    int day = 1; // start with day 1
-
-    for (int num : arr) {
-        if (sum + num > weight) {
-            day++;
-            sum = 0;
-        }
-        sum += num;
-
-        if (day > days) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-
-    int binary_search(int[] w,int days){
-        int[] arr=find_max(w);
-        int low=arr[0];
-        int high=arr[1];
-        int ans=high;
-        while(low<=high){
-
-            int mid=low + (high-low)/2;
-
-            if(check(w,mid,days)){
-                ans=mid;
-                high=mid-1;
-            }else{
-                low=mid+1;
+        for (int num : arr) {
+            if (sum + num > weight) {
+                day++; // need one more day
+                sum = 0;
             }
+            sum += num; // add current weight
 
+            if (day > days) {
+                return false; // can't ship in given days
+            }
         }
-    return ans;
+
+        return true; // shipping possible in given days
     }
 
+    // Binary search to find minimum possible max weight
+    public static int binary_search(int[] w, int days) {
+        int[] arr = find_max(w);
+        int low = arr[0]; // minimum capacity (at least max item)
+        int high = arr[1]; // maximum capacity (sum of all)
+        int ans = high;
 
-    public int shipWithinDays(int[] w, int days) {
-        return binary_search(w,days);
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // try middle capacity
+
+            if (check(w, mid, days)) {
+                ans = mid; // possible answer
+                high = mid - 1; // try for smaller capacity
+            } else {
+                low = mid + 1; // need more capacity
+            }
+        }
+
+        return ans;
+    }
+
+    // Main function to call from outside
+    public static int shipWithinDays(int[] w, int days) {
+        return binary_search(w, days);
     }
 }
