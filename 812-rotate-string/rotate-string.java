a34360a35a7b7b33bb20335511dc20466fb4db5a
Manav Desai <dmanavpianist@gmail.com>
2025-05-26 22:47:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (66.50%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,26 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String t) {
-
         if (s.length() != t.length()) return false;
-
-        StringBuilder sb = new StringBuilder(s);
-        for (int i = 0; i < s.length(); i++) {
-            if (sb.charAt(i) == t.charAt(0)) {
-                if (sb.toString().contains(t)) {
-                    return true;
-                } else {
-                     sb.append(sb.charAt(i));
-                }
-            }else{
-                sb.append(sb.charAt(i));
-            }
-        }
-
-        // if (sb.toString().equals(t)) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        return false;
+        return (s + s).contains(t);
     }
 }
