acf67a914b74390bc302eedf553953dfa2d8c188
Manav Desai <dmanavpianist@gmail.com>
2025-05-27 20:47:48 +0530

    Time: 605 ms (9.01%) | Memory: 45.1 MB (15.24%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -7,7 +7,6 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
-        // Traverse all values
         for (int value : map.values()) {
             if (value < min) min = value;
             if (value > max) max = value;
@@ -18,18 +17,19 @@ class Solution {
 
     public int beautySum(String s) {
         int sum = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        
+
         for (int i = 0; i < s.length(); i++) {
-            map.clear(); // reset for each substring starting at i
+            Map<Character, Integer> map = new HashMap<>(); // Reinitialize for each i
+
             for (int j = i; j < s.length(); j++) {
                 char k = s.charAt(j);
                 map.put(k, map.getOrDefault(k, 0) + 1);
 
-                int ans = calc(map);
-                sum += ans;
+                if (map.size() > 1) { // Only calculate beauty if there's variation
+                    int ans = calc(map);
+                    sum += ans;
+                }
             }
-            map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
         }
 
         return sum;
