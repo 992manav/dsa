f96a43a3703723090ae740d23506ef26d4bcc02e
Manav Desai <dmanavpianist@gmail.com>
2025-06-03 20:34:11 +0530

    Time: 1 ms (51.83%) | Memory: 43 MB (18.15%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,18 @@
 class Solution {
     List<List<Integer>> final_list = new ArrayList<>();
 
-    void fun(List<Integer> lst, int[] nums, int starting_index) {
-        final_list.add(new ArrayList<>(lst));
+    void backtrack(int[] nums, int index, List<Integer> current) {
+        final_list.add(new ArrayList<>(current)); // ✅ Add a copy of the current subset
 
-        for (int i = starting_index; i < nums.length; i++) {
-            lst.add(nums[i]);
-            fun(lst, nums, i + 1);
-            lst.remove(lst.size() - 1); // backtrack
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);          // Choose
+            backtrack(nums, i + 1, current); // Explore
+            current.remove(current.size() - 1); // Un-choose (backtrack)
         }
     }
 
     public List<List<Integer>> subsets(int[] nums) {
-        fun(new ArrayList<>(), nums, 0);
+        backtrack(nums, 0, new ArrayList<>());
         return final_list;
     }
 }
