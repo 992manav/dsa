b7c7af927fa8d3fb2e1e5c8dce3e8c68cb6cad89
Manav Desai <dmanavpianist@gmail.com>
2025-06-03 18:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (82.74%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,34 +1,34 @@
 import java.util.*;
 
 class Solution {
-    
+
     List<String> lst = new ArrayList<>();
 
-    void fun(StringBuilder s, String c, int l, int r, int equal) {
-        s.append(c);
-        if (c.equals("(")) {
-            l--;
-            equal--;
-        } else {
-            r--;
-            equal++;
-        }
-
+    void fun(StringBuilder s, int l, int r, int balance) {
         if (l == 0 && r == 0) {
             lst.add(s.toString());
             return;
         }
 
-        if (equal < 0 && r > 0) {
-            fun(new StringBuilder(s), ")", l, r, equal);
-        }
+        // Try adding '('
         if (l > 0) {
-            fun(new StringBuilder(s), "(", l, r, equal);
+            s.append('(');
+            fun(s, l - 1, r, balance - 1);
+            s.deleteCharAt(s.length() - 1); // backtrack
+        }
+
+        // Try adding ')' only if it won't unbalance
+        if (r > 0 && balance < 0) {
+            s.append(')');
+            fun(s, l, r - 1, balance + 1);
+            s.deleteCharAt(s.length() - 1); // backtrack
         }
     }
-    
+
     public List<String> generateParenthesis(int n) {
-        fun(new StringBuilder(), "(", n, n, 0);
+        StringBuilder sb = new StringBuilder();
+        sb.append('(');
+        fun(sb, n - 1, n, -1); // initial call with one '(' used
         return lst;
     }
 }
