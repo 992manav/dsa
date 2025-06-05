fde0376323f8b8609b690c1168c28b04e5688e1e
Manav Desai <dmanavpianist@gmail.com>
2025-06-05 23:43:22 +0530

    Time: 8 ms (55.67%) | Memory: 56.7 MB (74.18%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
 
-    boolean check_pali(StringBuilder s) {
+    boolean check_pali(String s) {
         int i = 0;
         int j = s.length() - 1;
 
@@ -14,33 +14,32 @@ class Solution {
             if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
-            i++; 
-            j--; 
+            i++;
+            j--;
         }
         return true;
     }
 
     List<List<String>> final_lst = new ArrayList<>();
 
-    void fun(StringBuilder sb, String s, int index, List<String> lst) {
-
-        if (index == s.length()) { 
-            final_lst.add(new ArrayList<>(lst)); 
+    void fun(String s, int index, List<String> lst) {
+        if (index == s.length()) {
+            final_lst.add(new ArrayList<>(lst));
             return;
         }
 
         for (int i = index; i < s.length(); i++) {
-            StringBuilder sub = new StringBuilder(s.substring(index, i + 1));
+            String sub = s.substring(index, i + 1);
             if (check_pali(sub)) {
-                lst.add(sub.toString());
-                fun(sb, s, i + 1, lst);
-                lst.remove(lst.size() - 1); 
+                lst.add(sub);
+                fun(s, i + 1, lst);
+                lst.remove(lst.size() - 1); // backtrack
             }
         }
     }
 
     public List<List<String>> partition(String s) {
-        fun(new StringBuilder(), s, 0, new ArrayList<>());
+        fun(s, 0, new ArrayList<>());
         return final_lst;
     }
 }
