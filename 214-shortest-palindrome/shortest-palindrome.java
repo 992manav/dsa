904488e0634b8e4b2bde616399cf2e9d0839d19c
Manav Desai <dmanavpianist@gmail.com>
2025-06-06 11:40:41 +0530

    Time: 6 ms (88.54%) | Memory: 44.8 MB (93.72%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,49 +1,32 @@
 class Solution {
-    static final long mod = 1000000007;
-    static final long base = 131;
-
-    // get substring hash from prefix hashes
-    private long getHash(long[] prefixHash, long[] power, int l, int r) {
-        long hash = prefixHash[r + 1] - (prefixHash[l] * power[r - l + 1]) % mod;
-        if (hash < 0) hash += mod;
-        return hash;
-    }
-
     public String shortestPalindrome(String s) {
         int n = s.length();
         if (n == 0) return s;
 
-        long[] power = new long[n + 1];
-        power[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            power[i] = (power[i - 1] * base) % mod;
-        }
+        long base = 131; // a random base
+        long mod = 1000000007; // a large prime
+        long hash_forward = 0;
+        long hash_backward = 0;
+        long power = 1;
+        int longestPalindromePrefix = 0;
 
-        long[] prefixHash = new long[n + 1];
         for (int i = 0; i < n; i++) {
-            prefixHash[i + 1] = (prefixHash[i] * base + (s.charAt(i) - 'a' + 1)) % mod;
-        }
+            int ch = s.charAt(i) - 'a' + 1;
 
-        // For suffix hash, do similar but reversed string
-        String rev = new StringBuilder(s).reverse().toString();
-        long[] suffixHash = new long[n + 1];
-        for (int i = 0; i < n; i++) {
-            suffixHash[i + 1] = (suffixHash[i] * base + (rev.charAt(i) - 'a' + 1)) % mod;
-        }
+            hash_forward = (hash_forward * base + ch) % mod;
+            hash_backward = (hash_backward + power * ch) % mod;
 
-        int longestPali = 0;
-        for (int len = n; len >= 1; len--) {
-            long leftHash = getHash(prefixHash, power, 0, len - 1);
-            long rightHash = getHash(suffixHash, power, n - len, n - 1);
-
-            if (leftHash == rightHash) {
-                longestPali = len;
-                break;
+            if (hash_forward == hash_backward) {
+                longestPalindromePrefix = i + 1;
             }
+
+            power = (power * base) % mod;
         }
 
-        String suffix = s.substring(longestPali);
-        StringBuilder sb = new StringBuilder(suffix).reverse();
-        return sb.toString() + s;
+        // The part after the palindromic prefix
+        String suffix = s.substring(longestPalindromePrefix);
+        StringBuilder rev = new StringBuilder(suffix).reverse();
+
+        return rev.append(s).toString();
     }
 }
