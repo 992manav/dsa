acc7c23ab0251468080699152a9257464b646195
Manav Desai <dmanavpianist@gmail.com>
2025-06-10 20:42:01 +0530

    Time: 4 ms (42.08%) | Memory: 43.2 MB (71.69%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,32 +1,38 @@
 import java.util.*;
+
 class Solution {
-    int target;
-    List<List<Integer>> final_list = new ArrayList<>();
-    public void fun(List<Integer> lst, int sum, int[] nums, int starting_index) {
-        if (sum == target) {
-            List<Integer> copy = new ArrayList<>(lst);
-            final_list.add(copy);
-            return;
-        }
-        if ( sum>target || starting_index >= nums.length) {
-            return;
-        }
-        // Include current number and recurse
-        if (nums[starting_index] + sum <= target) {
-            lst.add(nums[starting_index]);
-            fun(lst, sum + nums[starting_index], nums, starting_index + 1); // Choose and move forward
-            lst.remove(lst.size() - 1); // Backtrack
-        }
-        while(starting_index + 1 < nums.length && nums[starting_index+1]==nums[starting_index]){
-            starting_index++;
-        }
-        fun(lst, sum, nums, starting_index + 1); // Skip current number
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> finalList = new ArrayList<>();
+        Arrays.sort(candidates);
+        combinationSumHelper(candidates, target, 0, new ArrayList<>(), 0, finalList);
+        return finalList;
     }
 
-    public List<List<Integer>> combinationSum2(int[] candidates, int tar) {
-        this.target = tar;
-        Arrays.sort(candidates); 
-        fun(new ArrayList<>(), 0, candidates, 0);
-        return final_list;
+    private static void combinationSumHelper(int[] nums, int target, int start, 
+                                          List<Integer> current, int sum, 
+                                          List<List<Integer>> result) {
+        if (sum == target) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        
+        if (sum > target || start >= nums.length) {
+            return;
+        }
+
+        // Include current number
+        if(nums[start]+sum<=target){
+
+        current.add(nums[start]);
+        combinationSumHelper(nums, target, start + 1, current, sum + nums[start], result);
+        current.remove(current.size() - 1);
+        }
+        // Skip duplicates
+        while (start + 1 < nums.length && nums[start] == nums[start + 1]) {
+            start++;
+        }
+        
+        // Exclude current number
+        combinationSumHelper(nums, target, start + 1, current, sum, result);
     }
 }
