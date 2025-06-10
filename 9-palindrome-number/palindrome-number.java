d41e1cc7b5fff0adefc9e7c90e8600e33b9f6735
Manav Desai <dmanavpianist@gmail.com>
2025-06-10 19:47:45 +0530

    Time: 5 ms (69.65%) | Memory: 44.2 MB (37.94%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,33 +1,22 @@
-class Solution { 
-
-
-
-    public boolean isPalindrome(int x) { 
-        if((x%11==0 && x>0 ) || (x<10 && x>=0) ){ 
-            if(x==1122 || x==123123 || x==1000021 || x==21120){
-                return false;
-            }
-            return true; 
-        } else { 
-            if(x>0){ 
-                int m = x; 
-                int ten = 1; 
-                while(m/10 != 0){ 
-                    ten = ten*10; 
-                    m = m/10; 
-                } 
-
-                while(x != 0){ 
-                    if(x/ten != x%10){ 
-                        return false; 
-                    } 
-                    x = x%ten; 
-                    x = x/10; 
-                    ten = ten/100; 
-                } 
-                return true; 
-            } 
-            return false; 
-        } 
-    } 
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers are not palindromes
+        if (x < 0) {
+            return false;
+        }
+        // Single-digit numbers are palindromes
+        if (x < 10) {
+            return true;
+        }
+        
+        int original = x;
+        int reversed = 0;
+        
+        while (x > 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return original == reversed;
+    }
 }
