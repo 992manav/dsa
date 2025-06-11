280fbfe8473fb59da34f4fc583377d2f8b83702e
Manav Desai <dmanavpianist@gmail.com>
2025-06-11 22:09:56 +0530

    Time: 4 ms (38.34%) | Memory: 44.9 MB (74.82%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -4,14 +4,14 @@ class Solution {
 
     List<List<String>> finalResult = new ArrayList<>();
 
-    void fun(char[][] board, List<Integer> columns, int index, int column_index) {
+    void fun(List<StringBuilder> board, List<Integer> columns, int index, int column_index) {
         int x = index;
 
-        if (index == board.length) {
-            // ✅ All queens placed, convert board to list of strings
+        if (index == board.size()) {
+            // ✅ All queens placed, convert board to List<String>
             List<String> temp = new ArrayList<>();
-            for (int i = 0; i < board.length; i++) {
-                temp.add(new String(board[i]));
+            for (StringBuilder row : board) {
+                temp.add(row.toString());
             }
             finalResult.add(temp);
             return;
@@ -23,12 +23,12 @@ class Solution {
 
         int y = columns.get(column_index);
         if (check(board, x, y)) {
-            board[x][y] = 'Q';
+            board.get(x).setCharAt(y, 'Q');
             columns.remove(column_index);
 
             fun(board, columns, index + 1, 0);
 
-            board[x][y] = '.';
+            board.get(x).setCharAt(y, '.');
             columns.add(column_index, y);
         }
 
@@ -36,33 +36,41 @@ class Solution {
         fun(board, columns, index, column_index + 1);
     }
 
-    boolean check(char[][] board, int row, int col) {
-        int n = board.length;
+    boolean check(List<StringBuilder> board, int row, int col) {
+        int n = board.size();
+
+        // Check column
+        for (int i = 0; i < row; i++) {
+            if (board.get(i).charAt(col) == 'Q') return false;
+        }
 
         // Check upper-left diagonal
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
+            if (board.get(i).charAt(j) == 'Q') return false;
         }
 
         // Check upper-right diagonal
         for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q') return false;
+            if (board.get(i).charAt(j) == 'Q') return false;
         }
 
         return true;
     }
 
     public List<List<String>> solveNQueens(int n) {
-        char[][] board = new char[n][n];
+        List<StringBuilder> board = new ArrayList<>();
         List<Integer> columns = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
+            StringBuilder row = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                row.append('.');
+            }
+            board.add(row);
             columns.add(i);
         }
 
         fun(board, columns, 0, 0);
-
         return finalResult;
     }
 }
