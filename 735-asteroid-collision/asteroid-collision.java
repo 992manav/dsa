6023c0058e89c03d34208794cf1379762f25404b
Manav Desai <dmanavpianist@gmail.com>
2025-06-17 14:28:51 +0530

    Time: 5 ms (84.49%) | Memory: 45.6 MB (20.53%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -4,22 +4,21 @@ class Solution {
     public int[] asteroidCollision(int[] arr) {
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                st.push(arr[i]);
-            } else {
-                while (!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(arr[i])) {
-                    st.pop();
+                int asteroid = arr[i];
+                if (asteroid > 0) {
+                    st.push(asteroid);
+                } else {
+                    int absAsteroid = -asteroid;
+                    while (!st.isEmpty() && st.peek() > 0 && st.peek() < absAsteroid) {
+                        st.pop();
+                    }
+                    if (!st.isEmpty() && st.peek() == absAsteroid) {
+                        st.pop(); 
+                    } else if (st.isEmpty() || st.peek() < 0) {
+                        st.push(asteroid);
+                    }
                 }
 
-                if (!st.isEmpty() && st.peek() == Math.abs(arr[i])) {
-                    st.pop();
-                    continue;
-                }
-
-                if (st.isEmpty() || st.peek() < 0) {
-                    st.push(arr[i]);
-                }
-            }
         }
 
         int[] result = new int[st.size()];
