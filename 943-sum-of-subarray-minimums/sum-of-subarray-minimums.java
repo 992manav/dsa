576af8b39dceff2368aa32974eddb09515ad9067
Manav Desai <dmanavpianist@gmail.com>
2025-06-19 15:15:15 +0530

    Time: 80 ms (7.20%) | Memory: 51 MB (60.37%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,82 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+class Solution {
+
+    static final int MOD = (int)1e9 + 7;
+
+    void fun_left(int[] left, int[] arr) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (st.isEmpty()) {
+                left[i] = 0;
+                st.push(i);
+            } else {
+
+                while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                    st.pop();
+                }
+
+                if (st.isEmpty()) {
+                    left[i] = 0;
+                    st.push(i);
+                } else {
+                    left[i] = st.peek() + 1;
+                    st.push(i);
+                }
+
+            }
+
+        }
+
+    }
+
+    void fun_right(int[] right, int[] arr) {
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            if (st.isEmpty()) {
+                right[i] = arr.length - 1;
+                st.push(i);
+            } else {
+
+                while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                    st.pop();
+                }
+
+                if (st.isEmpty()) {
+                    right[i] = arr.length - 1; 
+                    st.push(i);
+                } else {
+                    right[i] = st.peek() - 1;
+                    st.push(i);
+                }
+
+            }
+
+        }
+
+    }
+
+    public int sumSubarrayMins(int[] arr) {
+        int[] left = new int[arr.length];
+        int[] right = new int[arr.length];
+
+        fun_left(left, arr);
+        fun_right(right, arr);
+
+        long sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            long leftCount = i - left[i] + 1;
+            long rightCount = right[i] - i + 1;
+            long contribution = (arr[i] % MOD * leftCount % MOD * rightCount % MOD) % MOD;
+            sum = (sum + contribution) % MOD;
+        }
+
+        return (int) sum;
+    }
+}
