a9bfc92a44696bf621bfb6c0c7414029e6287076
Manav Desai <dmanavpianist@gmail.com>
2025-06-23 21:51:33 +0530

    Time: 1 ms (90.07%) | Memory: 45.1 MB (54.19%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -18,33 +18,32 @@ import java.util.*;
 
 class Solution {
 
-    Queue<TreeNode> q = new LinkedList<>();
     List<List<Integer>> final_lst = new ArrayList<>();
 
-    void bfs(Queue<TreeNode> q) {
+    void bfs(Queue<TreeNode> q, Queue<TreeNode> q2) {
+        if (q.isEmpty()) return;
+
+        List<Integer> level = new ArrayList<>();
 
         while (!q.isEmpty()) {
-            Queue<TreeNode> q2 = new LinkedList<>();
-            List<Integer> level = new ArrayList<>();
+            TreeNode node = q.remove();
+            level.add(node.val);
 
-            while (!q.isEmpty()) {
-                TreeNode node = q.remove();
-                level.add(node.val);
-
-                if (node.left != null) q2.add(node.left);
-                if (node.right != null) q2.add(node.right);
-            }
-
-            final_lst.add(level);
-
-            bfs(q2);
+            if (node.left != null) q2.add(node.left);
+            if (node.right != null) q2.add(node.right);
         }
+
+        final_lst.add(level);
+        bfs(q2, new LinkedList<>()); // recurse to next level
     }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
         if (root == null) return final_lst;
+
+        Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        bfs(q);
+        bfs(q, new LinkedList<>());
+
         return final_lst;
     }
 }
