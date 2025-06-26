7bd73c27d8369d946103843daf76f07247534473
Manav Desai <dmanavpianist@gmail.com>
2025-06-26 10:57:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (8.56%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,28 +1,21 @@
-import java.util.*;
-
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
+    List<Integer> lst = new ArrayList<>();
+    int max_level = 0;
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+    void travel_right(TreeNode root, int level) {
+        if (root == null) return;
 
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            TreeNode rightmost = null;
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode node = queue.poll();
-                rightmost = node;
-
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-            }
-
-            result.add(rightmost.val);  // add the last node of this level
+        if (level > max_level) {
+            lst.add(root.val);
+            max_level = level; 
         }
 
-        return result;
+        travel_right(root.right, level + 1);
+        travel_right(root.left, level + 1);
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        travel_right(root, 1);
+        return lst;
     }
 }
