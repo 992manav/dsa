29534762968fd2dff12f9c1e66a3d9ab6e430830
Manav Desai <dmanavpianist@gmail.com>
2025-06-26 14:08:36 +0530

    Time: 1 ms (12.31%) | Memory: 42.3 MB (6.53%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -1,21 +1,19 @@
-class Pair {
-    TreeNode node;
-    int dir;
-    char party;
-
-    Pair(TreeNode node, int dir, char party) {
-        this.node = node;
-        this.dir = dir;
-        this.party = party;
-    }
-}
-
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        class Pair {
+            TreeNode node;
+            byte party; // 0 = L, 1 = R, 2 = root
+
+            Pair(TreeNode node, byte party) {
+                this.node = node;
+                this.party = party;
+            }
+        }
+
         if (root == null) return true;
 
         Deque<Pair> dq = new ArrayDeque<>();
-        dq.add(new Pair(root, 0, 'r'));
+        dq.add(new Pair(root, (byte) 2));
 
         Deque<Pair> st = new ArrayDeque<>();
         Deque<Pair> q = new ArrayDeque<>();
@@ -26,12 +24,12 @@ class Solution {
             while (len > 0) {
                 if (len == 1) {
                     Pair p = dq.poll();
-                    if (p.party != 'r') return false;
+                    if (p.party != 2) return false;
 
                     if (p.node.left != null)
-                        dq.addLast(new Pair(p.node.left, -1, 'L'));
+                        dq.addLast(new Pair(p.node.left, (byte) 0));
                     if (p.node.right != null)
-                        dq.addLast(new Pair(p.node.right, 1, 'R'));
+                        dq.addLast(new Pair(p.node.right, (byte) 1));
 
                     len--;
                     continue;
@@ -41,16 +39,16 @@ class Solution {
                 Pair last = dq.pollLast();
                 len -= 2;
 
-                if (first.party != 'L' || last.party != 'R' || first.node.val != last.node.val)
+                if (first.party != 0 || last.party != 1 || first.node.val != last.node.val)
                     return false;
 
                 if ((first.node.left == null) != (last.node.right == null)) return false;
-                if (first.node.left != null) q.add(new Pair(first.node.left, -1, 'L'));
-                if (last.node.right != null) st.push(new Pair(last.node.right, 1, 'R'));
+                if (first.node.left != null) q.add(new Pair(first.node.left, (byte) 0));
+                if (last.node.right != null) st.push(new Pair(last.node.right, (byte) 1));
 
                 if ((first.node.right == null) != (last.node.left == null)) return false;
-                if (first.node.right != null) q.add(new Pair(first.node.right, 1, 'L'));
-                if (last.node.left != null) st.push(new Pair(last.node.left, -1, 'R'));
+                if (first.node.right != null) q.add(new Pair(first.node.right, (byte) 0));
+                if (last.node.left != null) st.push(new Pair(last.node.left, (byte) 1));
             }
 
             while (!q.isEmpty()) dq.addLast(q.poll());
