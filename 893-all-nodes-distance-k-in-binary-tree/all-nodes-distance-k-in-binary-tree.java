e9c76d8af14fd9f58e02317148ade67bfade9d37
Manav Desai <dmanavpianist@gmail.com>
2025-06-27 19:53:51 +0530

    Time: 10 ms (100.00%) | Memory: 42.5 MB (74.16%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -6,7 +6,7 @@ class Solution {
 
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         this.target = target;
-        this.dis=k;
+        this.dis = k;
         path(root, k);
         return lst;
     }
@@ -18,48 +18,36 @@ class Solution {
 
         if (k == 0 && found) {
             lst.add(root.val);
-            if(dis==0){
-                found=false;
-            }
+            if (dis == 0) found = false;
             return -2;
         }
 
         if (found) {
             path(root.left, k - 1);
             path(root.right, k - 1);
-            if (root == target) return k - 1;
-        } else {
-            int x = path(root.left, k);
+            return (root == target) ? k - 1 : -2;
+        }
 
-            if (x != -1) {
-                if (x == 0 ) {
-                    lst.add(root.val);
-                    return -2;
-                } else {
-                    if(x==-2){
-                        found=false;
-                    }
-                    path(root.right, x - 1);
-                    return x - 1;
-                }
-            } else {
-                int y = path(root.right, k);
-
-                if (y != -1) {
-                    if (y == 0) {
-                        lst.add(root.val);
-                        return -2;
-                    } else {
-                        if(y==-2){
-                            found=false;
-                        }
-                        path(root.left, y - 1);
-                        return y - 1;
-                    }
-                } else {
-                    return -1; // Target not found
-                }
+        int x = path(root.left, k);
+        if (x != -1) {
+            if (x == 0) {
+                lst.add(root.val);
+                return -2;
             }
+            if (x == -2) found = false;
+            path(root.right, x - 1);
+            return x - 1;
+        }
+
+        int y = path(root.right, k);
+        if (y != -1) {
+            if (y == 0) {
+                lst.add(root.val);
+                return -2;
+            }
+            if (y == -2) found = false;
+            path(root.left, y - 1);
+            return y - 1;
         }
 
         return -1;
