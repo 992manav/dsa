8f24c35548dc6aff6ecf309c2cdaaa5e41417813
Manav Desai <dmanavpianist@gmail.com>
2025-06-28 00:58:09 +0530

    Time: 4 ms (4.44%) | Memory: 45.6 MB (80.98%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,28 +1,42 @@
+import java.util.*;
+
 class Solution {
     public void setZeroes(int[][] matrix) {
 
         Set<Integer> sti = new HashSet<>();
         Set<Integer> stj = new HashSet<>();
 
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
 
-        // First pass: record positions of zeros
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0) {
                     sti.add(i);
                     stj.add(j);
+                    if (!map.containsKey(i)) {
+                        map.put(i, j); // changed `add` to `put`
+                    }
+                    if (!map2.containsKey(j)) {
+                        map2.put(j, i); // changed `add` to `put`
+                    }
+                } else {
+                    if (sti.contains(i) || stj.contains(j)) {
+                        matrix[i][j] = 0;
+                    }
                 }
             }
         }
 
-        // Second pass: set zeros based on recorded rows and columns
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (sti.contains(i) || stj.contains(j)) {
-                    matrix[i][j] = 0;
-                }
+        for (int row : sti) {
+            for (int j = 0; j < map.get(row); j++) {
+                matrix[row][j] = 0;
+            }
+        }
+
+        for (int col : stj) {
+            for (int i = 0; i < map2.get(col); i++) {
+                matrix[i][col] = 0;
             }
         }
     }
