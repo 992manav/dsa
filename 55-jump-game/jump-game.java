ce9bca75d9888253840cd45079d05b40013d75a5
Manav Desai <dmanavpianist@gmail.com>
2025-06-28 00:14:45 +0530

    Time: 8 ms (15.63%) | Memory: 45.5 MB (84.24%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,19 +1,17 @@
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 class Solution { 
     public boolean canJump(int[] nums) { 
-        Stack<Integer> st = new Stack<>();
-        st.push(nums.length - 1);
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(nums.length - 1);
         
         for (int i = nums.length - 2; i >= 0; i--) { 
-            if (i + nums[i] >= st.peek()) {
-                st.push(i);
+            if (i + nums[i] >= stack.peek()) {
+                stack.push(i);
             } 
 
-            if(i==0){
-                if (i + nums[i] < st.peek()) {
-                    return false;
-                } 
+            if (i == 0 && i + nums[i] < stack.peek()) {
+                return false;
             }
         } 
         return true; 
