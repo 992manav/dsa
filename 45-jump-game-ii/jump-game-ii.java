292387a8fbaf7596aebd6b2bdaeb5122dc7e1b0b
Manav Desai <dmanavpianist@gmail.com>
2025-06-28 22:59:42 +0530

    Time: 32 ms (18.91%) | Memory: 45.9 MB (8.63%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int jump(int[] nums) {
+        int nextPos = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) { 
+            if (i + nums[i] >= nextPos) {
+                nextPos = i;
+            }
+
+            if (i == 0 && i + nums[i] < nextPos) {
+                return -1; // changed 'false' to -1 since method returns int
+            }
+        } 
+
+        int[] a = new int[nums.length];
+        a[nums.length - 1] = 0; // fixed typo: 'legnth'
+
+        for (int i = nums.length - 2; i >= 0; i--) { 
+            int kitnamax = nums[i];
+
+            int min = Integer.MAX_VALUE; // fixed: 'Intger' → 'Integer'
+
+            if (kitnamax > 0) { // fixed typo: 'ktinamxax' → 'kitnamax'
+                for (int j = 1; j <= kitnamax && i + j < nums.length; j++) {
+                    if (a[i + j] < min) {
+                        min = a[i + j];
+                    }
+                }
+            }
+
+            a[i] = (min == Integer.MAX_VALUE) ? Integer.MAX_VALUE : min + 1;
+        } 
+
+        return a[0]; // changed 'true' to returning result
+    }
+}
