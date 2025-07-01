61e3f0d5601ac941c3adc4876b4905c1fd522518
Manav Desai <dmanavpianist@gmail.com>
2025-07-01 23:36:56 +0530

    Time: 3 ms (53.26%) | Memory: 42.3 MB (77.55%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,16 +1,16 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) return "0"; // Optimization: shortcut for zero
+        // Edge case optimization
+        if (num1.equals("0") || num2.equals("0")) return "0";
 
         int len1 = num1.length();
         int len2 = num2.length();
 
-        List<Integer> lst = new ArrayList<>();
-        for (int k = 0; k < len1 + len2; k++) {
-            lst.add(0);
-        }
+        // Using array instead of List<Integer> for better performance
+        int[] lst = new int[len1 + len2];
 
-        for (int j = len2 - 1; j >= 0; j--) {
+        int j = len2 - 1;
+        while (j >= 0) {
             int index = len2 - 1 - j;
             int below = num2.charAt(j) - '0';
             int carry = 0;
@@ -20,9 +20,9 @@ class Solution {
                 int upper = num1.charAt(i) - '0';
                 int mul = upper * below + carry;
 
-                int sum = lst.get(index) + (mul % 10);
-                lst.set(index, sum % 10);
-                lst.set(index + 1, lst.get(index + 1) + sum / 10);
+                int sum = lst[index] + (mul % 10);
+                lst[index] = sum % 10;
+                lst[index + 1] += sum / 10;
 
                 carry = mul / 10;
                 index++;
@@ -30,16 +30,23 @@ class Solution {
             }
 
             if (carry != 0) {
-                lst.set(index, lst.get(index) + carry);
+                lst[index] += carry;
             }
+            j--;
         }
 
+        // Build final result string
         StringBuilder sb = new StringBuilder();
+        int k = lst.length - 1;
 
-        int k = lst.size() - 1;
-        while (k > 0 && lst.get(k) == 0) k--;
+        // Skip leading zeros
+        while (k > 0 && lst[k] == 0) {
+            k--;
+        }
 
-        while (k >= 0) sb.append(lst.get(k--));
+        while (k >= 0) {
+            sb.append(lst[k--]);
+        }
 
         return sb.toString();
     }
