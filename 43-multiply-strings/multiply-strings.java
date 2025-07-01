dc3c2b1a616fca72e69c3ccbea97e4e180aa92d9
Manav Desai <dmanavpianist@gmail.com>
2025-07-01 23:34:20 +0530

    Time: 5 ms (32.73%) | Memory: 43.1 MB (16.71%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,28 +1,26 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        int i = num1.length() - 1;
-        int j = num2.length() - 1;
+        if (num1.equals("0") || num2.equals("0")) return "0"; // Optimization: shortcut for zero
+
+        int len1 = num1.length();
+        int len2 = num2.length();
 
         List<Integer> lst = new ArrayList<>();
-        // Initialize lst with enough space
-        for (int k = 0; k < num1.length() + num2.length(); k++) {
+        for (int k = 0; k < len1 + len2; k++) {
             lst.add(0);
         }
 
-        int index = 0;
-        int carry = 0;
-
-        while (j >= 0) {
-            index = num2.length() - 1 - j;
+        for (int j = len2 - 1; j >= 0; j--) {
+            int index = len2 - 1 - j;
             int below = num2.charAt(j) - '0';
-            carry = 0;
-            i = num1.length() - 1;
+            int carry = 0;
+            int i = len1 - 1;
 
             while (i >= 0) {
                 int upper = num1.charAt(i) - '0';
                 int mul = upper * below + carry;
 
-                int sum = lst.get(index) + mul % 10;
+                int sum = lst.get(index) + (mul % 10);
                 lst.set(index, sum % 10);
                 lst.set(index + 1, lst.get(index + 1) + sum / 10);
 
@@ -34,20 +32,14 @@ class Solution {
             if (carry != 0) {
                 lst.set(index, lst.get(index) + carry);
             }
-            j--;
         }
 
         StringBuilder sb = new StringBuilder();
 
-        // Remove leading zeros
         int k = lst.size() - 1;
-        while (k > 0 && lst.get(k) == 0) {
-            k--;
-        }
+        while (k > 0 && lst.get(k) == 0) k--;
 
-        for (; k >= 0; k--) {
-            sb.append(lst.get(k));
-        }
+        while (k >= 0) sb.append(lst.get(k--));
 
         return sb.toString();
     }
