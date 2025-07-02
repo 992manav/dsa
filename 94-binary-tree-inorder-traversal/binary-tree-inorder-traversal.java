87b5c73b76d49d7c2786496d712e7aea068df192
Manav Desai <dmanavpianist@gmail.com>
2025-07-03 00:27:06 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (15.45%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,48 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 import java.util.*;
 
 class Solution {
 
     public List<Integer> inorderTraversal(TreeNode root) {
+        TreeNode node = root;
+
         List<Integer> lst = new ArrayList<>();
+        while (node != null) {
 
-        Stack<TreeNode> st = new Stack<>();
+            TreeNode temp = node.left;
 
-        if (root == null) return lst; 
+            if (temp != null) {
+                while (temp.right != null && temp.right != node) {
+                    temp = temp.right;
+                }
 
-        st.push(root);
-        //left root right
-
-        while (!st.isEmpty()) {
-            TreeNode node = st.peek();
-
-            if (node.left != null) {
-                st.push(node.left);
-                node.left=null;
-                continue;
+                if (temp.right == null) {
+                    temp.right = node;
+                    node = node.left;
+                } else {
+                    temp.right = null;
+                    lst.add(node.val);
+                    node = node.right;
+                }
+            } else {
+                lst.add(node.val);
+                node = node.right;
             }
-
-            lst.add(st.pop().val);
-
-            if (node.right != null) {
-                st.push(node.right);
-            }
-
-            
         }
 
         return lst;
