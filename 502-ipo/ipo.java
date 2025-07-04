93aa5a08fc216fffffa4bf844fd3782f4a384512
Manav Desai <dmanavpianist@gmail.com>
2025-07-05 00:38:20 +0530

    Time: 1051 ms (5.03%) | Memory: 60.1 MB (86.35%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,19 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
 
-        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
 
         for (int i = 0; i < profits.length; i++) {
             map.computeIfAbsent(profits[i], key -> new ArrayList<>()).add(capital[i]);
         }
 
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys, Collections.reverseOrder());
+
         while (k > 0) {
             boolean found = false;
 
             // Iterate over profits in descending order
-            for (var entry : map.descendingMap().entrySet()) {
-                int profit = entry.getKey();
-                List<Integer> lst = entry.getValue();
+            for (int profit : keys) {
+                List<Integer> lst = map.get(profit); // fix: get list from map
 
                 // Use iterator for safe in-place removal
                 Iterator<Integer> itr = lst.iterator();
