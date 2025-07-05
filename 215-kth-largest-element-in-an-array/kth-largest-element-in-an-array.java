e1d4b698b842f04675421133b8434e246f86c3a3
Manav Desai <dmanavpianist@gmail.com>
2025-07-05 22:08:04 +0530

    Time: 23 ms (83.68%) | Memory: 57.9 MB (65.29%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,18 +1,51 @@
 class Solution {
+
+    // Heapify down within a limited heap size
+    void heapifyDown(int[] nums, int i, int heapSize) {
+        while (true) {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < heapSize && nums[left] > nums[largest]) {
+                largest = left;
+            }
+
+            if (right < heapSize && nums[right] > nums[largest]) {
+                largest = right;
+            }
+
+            if (largest == i) break;
+
+            int temp = nums[largest];
+            nums[largest] = nums[i];
+            nums[i] = temp;
+
+            i = largest;
+        }
+    }
+
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        int n = nums.length;
 
-
-        for (int num : nums) {
-            pq.offer(num);
+        // Step 1: Build Max-Heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapifyDown(nums, i, n);
         }
 
-        k--;
-        while (k > 0) {
-            pq.poll(); 
-            k--;
+       
+        int heapSize = n;
+        for (int j = 0; j < k - 1; j++) {
+         
+            int temp = nums[0];
+            nums[0] = nums[heapSize - 1];
+            nums[heapSize - 1] = temp;
+
+            heapSize--; // reduce heap size
+            heapifyDown(nums, 0, heapSize);
         }
 
-        return pq.poll();
+       
+        return nums[0];
     }
 }
