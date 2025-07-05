5bf6acd3bbeb9cd748e7934424a76b77609d4cd8
Manav Desai <dmanavpianist@gmail.com>
2025-07-05 22:32:13 +0530

    Time: 4 ms (97.06%) | Memory: 56.5 MB (97.29%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,51 +1,27 @@
 class Solution {
 
-    // Heapify down within a limited heap size
-    void heapifyDown(int[] nums, int i, int heapSize) {
-        while (true) {
-            int largest = i;
-            int left = 2 * i + 1;
-            int right = 2 * i + 2;
-
-            if (left < heapSize && nums[left] > nums[largest]) {
-                largest = left;
-            }
-
-            if (right < heapSize && nums[right] > nums[largest]) {
-                largest = right;
-            }
-
-            if (largest == i) break;
-
-            int temp = nums[largest];
-            nums[largest] = nums[i];
-            nums[i] = temp;
-
-            i = largest;
-        }
-    }
-
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
+        int[] count = new int[20001]; // for range [-10000, 10000]
+        Arrays.fill(count, 0);
 
-        // Step 1: Build Max-Heap
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapifyDown(nums, i, n);
+        int max_index = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int idx = nums[i] + 10000;
+            count[idx] += 1;
+
+            if (idx > max_index) {
+                max_index = idx;
+            }
         }
 
-       
-        int heapSize = n;
-        for (int j = 0; j < k - 1; j++) {
-         
-            int temp = nums[0];
-            nums[0] = nums[heapSize - 1];
-            nums[heapSize - 1] = temp;
-
-            heapSize--; // reduce heap size
-            heapifyDown(nums, 0, heapSize);
+        for (int i = max_index; i >= 0; i--) {
+            k -= count[i];
+            if (k <= 0) {
+                return i - 10000; 
+            }
         }
 
-       
-        return nums[0];
+        return -1;
     }
 }
