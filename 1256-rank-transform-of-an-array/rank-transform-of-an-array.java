5f59d7e8afd6416f81e4253610d23a8628c76882
Manav Desai <dmanavpianist@gmail.com>
2025-07-06 01:22:37 +0530

    Time: 36 ms (38.08%) | Memory: 58.1 MB (94.93%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,27 +1,24 @@
 public class Solution {
     public int[] arrayRankTransform(int[] arr) {
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Set<Integer> set = new HashSet<>();
         for (int num : arr) {
-            pq.add(num);
+            set.add(num);
         }
 
+        List<Integer> sorted = new ArrayList<>(set);
+        Collections.sort(sorted); // O(k log k)
+
         HashMap<Integer, Integer> map = new HashMap<>();
-
         int rank = 1;
-        while (!pq.isEmpty()) {
-            int p = pq.poll();
-
-            if (!map.containsKey(p)) {
-                map.put(p, rank);
-                rank++;
-            }
+        for (int num : sorted) {
+            map.put(num, rank++);
         }
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = map.get(arr[i]);
         }
 
-        return arr; 
+        return arr;
     }
 }
