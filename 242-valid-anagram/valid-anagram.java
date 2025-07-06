64c22542567fa5140b07c4a979b88649f6e7847f
Manav Desai <dmanavpianist@gmail.com>
2025-07-06 18:15:20 +0530

    Time: 9 ms (32.55%) | Memory: 43.2 MB (76.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,34 @@
-import java.util.Arrays;
+public class Solution {
 
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        
-        return Arrays.equals(sChars, tChars);
+    public static boolean isAnagram(String s, String t) {
+        // Quick length check
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int xor = 0;
+        int xor2 = 0;
+        int[] count = new int[26];
+
+        // Build both xor‑sums and frequency balance in one pass
+        for (int i = 0; i < s.length(); i++) {
+            xor  ^= s.charAt(i) - 'a';
+            xor2 ^= t.charAt(i) - 'a';
+
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        // If the xor‑sums match, verify the counts
+        if (xor == xor2) {
+            for (int c : count) {
+                if (c != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
     }
 }
