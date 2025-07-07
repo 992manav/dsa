75f4e8c413500b6ce2e1e1a642db98a51cddfebf
Manav Desai <dmanavpianist@gmail.com>
2025-07-07 23:37:17 +0530

    Time: 4 ms (51.32%) | Memory: 48.8 MB (97.22%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,64 +1,54 @@
-class Pair {
-    int row;
-    int col;
-
-    Pair(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
-}
-
 class Solution {
 
-    void bfs(char[][] grid, Queue<Pair> q, int row, int col) {
-        q.offer(new Pair(row, col));
+    void bfs(char[][] grid, Queue<Integer> q, int row, int col, int cols) {
+        q.offer(row * cols + col);
         grid[row][col] = '0'; // mark as visited
 
         int n = grid.length;
         int m = grid[0].length;
 
         while (!q.isEmpty()) {
-            Pair p = q.poll();
-            int r = p.row;
-            int c = p.col;
+            int code = q.poll();
+            int r = code / cols;
+            int c = code % cols;
 
             // up
             if (r - 1 >= 0 && grid[r - 1][c] == '1') {
                 grid[r - 1][c] = '0';
-                q.offer(new Pair(r - 1, c));
+                q.offer((r - 1) * cols + c);
             }
 
             // down
             if (r + 1 < n && grid[r + 1][c] == '1') {
                 grid[r + 1][c] = '0';
-                q.offer(new Pair(r + 1, c));
+                q.offer((r + 1) * cols + c);
             }
 
             // left
             if (c - 1 >= 0 && grid[r][c - 1] == '1') {
                 grid[r][c - 1] = '0';
-                q.offer(new Pair(r, c - 1));
+                q.offer(r * cols + (c - 1));
             }
 
             // right
             if (c + 1 < m && grid[r][c + 1] == '1') {
                 grid[r][c + 1] = '0';
-                q.offer(new Pair(r, c + 1));
+                q.offer(r * cols + (c + 1));
             }
         }
     }
 
     public int numIslands(char[][] grid) {
-        Queue<Pair> q = new LinkedList<>();
-        int count = 0;
         int n = grid.length;
         int m = grid[0].length;
+        Queue<Integer> q = new LinkedList<>();
+        int count = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    bfs(grid, q, i, j);
+                    bfs(grid, q, i, j, m);
                 }
             }
         }
