5155c8482189935735a3fcd06fbf99c830037a45
Manav Desai <dmanavpianist@gmail.com>
2025-07-15 13:40:52 +0530

    Time: 4 ms (98.50%) | Memory: 55.7 MB (26.24%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,14 +1,12 @@
 class Solution {
 
     boolean[] visited;
-    boolean[] terminal;
     boolean[] safe;
 
     boolean dfs(int[][] graph, int node) {
-
         visited[node] = true;
 
-        if (terminal[node]) {
+        if (graph[node].length == 0) {
             safe[node] = true;
             return true;
         }
@@ -17,21 +15,12 @@ class Solution {
             int neighbour = graph[node][j];
 
             if (visited[neighbour]) {
-
-                if (safe[neighbour]) {
-                    continue;
-                } else {
+                if (!safe[neighbour]) {
                     return false;
                 }
-
             } else {
-
-                if (safe[neighbour]) {
-                    continue;
-                } else {
-                    if (!dfs(graph, neighbour)) {
-                        return false;
-                    }
+                if (!dfs(graph, neighbour)) {
+                    return false;
                 }
             }
         }
@@ -42,26 +31,18 @@ class Solution {
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
 
-        visited = new boolean[graph.length];
-        terminal = new boolean[graph.length];
-        safe = new boolean[graph.length];
+        int n = graph.length;
+        visited = new boolean[n];
+        safe = new boolean[n];
 
-        for (int i = 0; i < terminal.length; i++) {
-            if (graph[i].length == 0) {
-                terminal[i] = true;
-            } else {
-                terminal[i] = false;
-            }
-        }
-
-        for (int i = 0; i < visited.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 dfs(graph, i);
             }
         }
 
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < safe.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (safe[i]) {
                 result.add(i);
             }
