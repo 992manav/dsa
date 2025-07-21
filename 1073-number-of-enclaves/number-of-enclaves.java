43c09a619f2857c827a3d27fd633257d6b97618d
Manav Desai <dmanavpianist@gmail.com>
2025-07-21 23:58:08 +0530

    Time: 12 ms (12.53%) | Memory: 51.7 MB (94.53%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -4,10 +4,8 @@ class Solution {
     int[][] board;
     int rows, cols;
 
-    void bfs(int i, int j) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i, j});
-        board[i][j] = 0;
+    void multisourceBFS(List<int[]> sources) {
+        Queue<int[]> q = new LinkedList<>(sources);
 
         int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
@@ -33,18 +31,35 @@ class Solution {
         rows = board.length;
         cols = board[0].length;
 
-        // Remove all land connected to boundary
+        List<int[]> sources = new ArrayList<>();
+
+        // Collect all boundary land cells (sources)
         for (int j = 0; j < cols; j++) {
-            if (board[0][j] == 1) bfs(0, j);
-            if (board[rows - 1][j] == 1) bfs(rows - 1, j);
+            if (board[0][j] == 1) {
+                board[0][j] = 0;
+                sources.add(new int[]{0, j});
+            }
+            if (board[rows - 1][j] == 1) {
+                board[rows - 1][j] = 0;
+                sources.add(new int[]{rows - 1, j});
+            }
         }
 
         for (int i = 0; i < rows; i++) {
-            if (board[i][0] == 1) bfs(i, 0);
-            if (board[i][cols - 1] == 1) bfs(i, cols - 1);
+            if (board[i][0] == 1) {
+                board[i][0] = 0;
+                sources.add(new int[]{i, 0});
+            }
+            if (board[i][cols - 1] == 1) {
+                board[i][cols - 1] = 0;
+                sources.add(new int[]{i, cols - 1});
+            }
         }
 
-        // Count remaining land cells
+        // Perform multisource flood fill
+        multisourceBFS(sources);
+
+        // Count remaining land
         int count = 0;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
