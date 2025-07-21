bfdf4d26764cb3d58f22c4b5e88cadddf10f7969
Manav Desai <dmanavpianist@gmail.com>
2025-07-21 21:30:30 +0530

    Time: 2 ms (82.89%) | Memory: 45.7 MB (26.17%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -2,66 +2,70 @@ import java.util.*;
 
 class Solution {
 
-    List<int[]> final_lst = new ArrayList<>();
     char[][] board;
     int rows, cols;
 
     void bfs(int i, int j) {
-        board[i][j] = 'X';
-        Deque<int[]> q = new ArrayDeque<>();
-        List<int[]> lst = new ArrayList<>();
+        Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{i, j});
-        lst.add(new int[]{i, j});
-
-        boolean flag = false;
+        board[i][j] = '#';
 
         int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         while (!q.isEmpty()) {
-            int[] p = q.remove();
-            int r = p[0], c = p[1];
-
-            if (!flag && isEdge(r, c)) {
-                flag = true;
-            }
+            int[] cell = q.remove();
+            int r = cell[0];
+            int c = cell[1];
 
             for (int[] d : directions) {
                 int nr = r + d[0];
                 int nc = c + d[1];
 
                 if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && board[nr][nc] == 'O') {
-                    board[nr][nc] = 'X';
-                    int[] next = new int[]{nr, nc};
-                    lst.add(next);
-                    q.add(next);
+                    board[nr][nc] = '#';
+                    q.add(new int[]{nr, nc});
                 }
             }
         }
-
-        if (flag) {
-            final_lst.addAll(lst);
-        }
-    }
-
-    boolean isEdge(int i, int j) {
-        return i == 0 || j == 0 || i == rows - 1 || j == cols - 1;
     }
 
     public void solve(char[][] board) {
         this.board = board;
-        this.rows = board.length;
-        this.cols = board[0].length;
+        rows = board.length;
+        cols = board[0].length;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (board[i][j] == 'O') {
-                    bfs(i, j);
-                }
+        for (int j = 0; j < cols; j++) {
+            if (board[0][j] == 'O') {
+                bfs(0, j);
             }
         }
 
-        for (int[] p : final_lst) {
-            board[p[0]][p[1]] = 'O';
+        for (int j = 0; j < cols; j++) {
+            if (board[rows - 1][j] == 'O') {
+                bfs(rows - 1, j);
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O') {
+                bfs(i, 0);
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            if (board[i][cols - 1] == 'O') {
+                bfs(i, cols - 1);
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
         }
     }
 }
