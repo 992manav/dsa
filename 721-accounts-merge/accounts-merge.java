38e407761e05988caec973b1f64c0b4625e0e05f
Manav Desai <dmanavpianist@gmail.com>
2025-08-02 11:24:35 +0530

    Time: 196 ms (5.01%) | Memory: 48.4 MB (44.46%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -34,6 +34,8 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             String name1 = acc.get(i).get(0);
+            Set<String> emails1 = new HashSet<>(acc.get(i).subList(1, acc.get(i).size()));
+
             for (int j = i + 1; j < n; j++) {
                 if (!acc.get(j).get(0).equals(name1)) continue;
 
@@ -41,15 +43,18 @@ class Solution {
                 int p2 = find_parent(j);
                 if (p1 == p2) continue;
 
-                List<String> list1 = acc.get(i);
-                List<String> list2 = acc.get(j);
-
-                Set<String> set1 = new HashSet<>(list1.subList(1, list1.size()));
-                for (int k = 1; k < list2.size(); k++) {
-                    if (set1.contains(list2.get(k))) {
+                List<String> acc2 = acc.get(j);
+                boolean matched = false;
+                for (int k = 1; k < acc2.size(); k++) {
+                    if (emails1.contains(acc2.get(k))) {
                         unionByRank(p1, p2);
+                        matched = true;
                         break;
                     }
+                }
+                // Optionally update emails1 set to reduce future checks
+                if (matched) {
+                    emails1.addAll(acc2.subList(1, acc2.size()));
                 }
             }
         }
@@ -58,30 +63,31 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             int p = find_parent(i);
-            map.putIfAbsent(p, new ArrayList<>());
-            map.get(p).add(i);
+            map.computeIfAbsent(p, k -> new ArrayList<>()).add(i);
         }
 
         List<List<String>> final_lst = new ArrayList<>();
 
         for (int key : map.keySet()) {
-            List<String> lst = new ArrayList<>();
-            List<Integer> indices = map.get(key);
-            lst.add(acc.get(indices.get(0)).get(0));
-
+            List<Integer> groupIndices = map.get(key);
             Set<String> emailSet = new HashSet<>();
-            for (int idx : indices) {
-                List<String> emails = acc.get(idx);
-                for (int j = 1; j < emails.size(); j++) {
-                    emailSet.add(emails.get(j));
+            String name = acc.get(groupIndices.get(0)).get(0);
+
+            for (int idx : groupIndices) {
+                List<String> accEntry = acc.get(idx);
+                for (int j = 1; j < accEntry.size(); j++) {
+                    emailSet.add(accEntry.get(j));
                 }
             }
 
-            List<String> emailList = new ArrayList<>(emailSet);
-            Collections.sort(emailList);
-            lst.addAll(emailList);
+            List<String> merged = new ArrayList<>();
+            merged.add(name);
 
-            final_lst.add(lst);
+            List<String> sortedEmails = new ArrayList<>(emailSet);
+            Collections.sort(sortedEmails);
+            merged.addAll(sortedEmails);
+
+            final_lst.add(merged);
         }
 
         return final_lst;
