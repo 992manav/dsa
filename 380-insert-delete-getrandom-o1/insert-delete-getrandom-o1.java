d17accf395bcbf38246ef53f2eb463f05f6e2851
Manav Desai <dmanavpianist@gmail.com>
2025-08-02 13:45:02 +0530

    Time: 28 ms (55.64%) | Memory: 95.7 MB (5.79%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -2,32 +2,52 @@ import java.util.*;
 
 class RandomizedSet {
 
-    Set<Integer> set;
+    Map<Integer, Integer> indexToValue;
+    Map<Integer, Integer> valueToIndex;
+    int currentIndex;
+    Random rand;
 
     public RandomizedSet() {
-        set = new HashSet<>();
+        indexToValue = new HashMap<>();
+        valueToIndex = new HashMap<>();
+        currentIndex = 0;
+        rand = new Random();
     }
-    
+
     public boolean insert(int val) {
-        if (set.contains(val)) {
+        if (valueToIndex.containsKey(val)) {
             return false;
-        } else {
-            set.add(val);
-            return true;
         }
+
+        indexToValue.put(currentIndex, val);
+        valueToIndex.put(val, currentIndex);
+        currentIndex++;
+        return true;
     }
-    
+
     public boolean remove(int val) {
-        if (set.contains(val)) {
-            set.remove(val);
-            return true;
-        } else {
+        if (!valueToIndex.containsKey(val)) {
             return false;
         }
+
+        int indexToRemove = valueToIndex.get(val);
+        int lastIndex = currentIndex - 1;
+        int lastVal = indexToValue.get(lastIndex);
+
+        // Swap last element with element to be removed
+        indexToValue.put(indexToRemove, lastVal);
+        valueToIndex.put(lastVal, indexToRemove);
+
+        // Remove last element
+        indexToValue.remove(lastIndex);
+        valueToIndex.remove(val);
+
+        currentIndex--;
+        return true;
     }
-    
+
     public int getRandom() {
-        int n = (int)(Math.random() * set.size());
-        return new ArrayList<>(set).get(n);
+        int randomIndex = rand.nextInt(currentIndex);
+        return indexToValue.get(randomIndex);
     }
 }
