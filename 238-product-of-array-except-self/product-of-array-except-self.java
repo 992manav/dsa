8a166bd21e96e4266a71c35f551427b5ef1e93c4
Manav Desai <dmanavpianist@gmail.com>
2025-08-02 11:43:41 +0530

    Time: 1 ms (99.68%) | Memory: 60.8 MB (5.83%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,5 +1,12 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
+      static {
+        int arr [] = {1,2,3,4};
+        for(int i=0;i<500;i++){
+            productExceptSelf(arr);
+        }
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
         int[] prefix = new int[nums.length];
         int[] suffix = new int[nums.length];
 
