28373d4e716f226190a05abdf05bfcdeb80089aa
Manav Desai <dmanavpianist@gmail.com>
2025-08-09 02:41:42 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (96.10%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,25 +1,23 @@
+import java.util.Arrays;
+
 class Solution {
     int[] dp;
 
-    int fun(int[] nums, int index) {
-        if (index >= nums.length) return 0;
-
-        if (dp[index] != -1) return dp[index];
-
-        // Choice 1: skip current house
-        int skip = fun(nums, index + 1);
-
-        // Choice 2: take current house
-        int take = nums[index] + fun(nums, index + 2);
-
-        // Store max in dp and return
-        return dp[index] = Math.max(skip, take);
-    }
-
     public int rob(int[] nums) {
         dp = new int[nums.length];
-        Arrays.fill(dp, -1); // Initialize all with -1
+        Arrays.fill(dp, -1);
+        int n = nums.length;
 
-        return fun(nums, 0); // Start from index 0
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[0], nums[1]);
+
+        dp[n - 1] = nums[n - 1];
+        dp[n - 2] = Math.max(nums[n - 2], dp[n - 1]);
+
+        for (int i = n - 3; i >= 0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
+
+        return dp[0];
     }
 }
