39e58f24174440b34af4a1094846479e9bb7741d
Manav Desai <dmanavpianist@gmail.com>
2025-08-11 12:58:50 +0530

    Time: 18 ms (81.72%) | Memory: 106.5 MB (5.38%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    static final int MOD = (int) (1e9 + 7);
+    static final int MOD = (int) 1e9 + 7;
     static int[] arr = new int[30];
     static {
         for (int i = 0; i < 30; i++) {
@@ -23,52 +23,39 @@ class Solution {
         return ans;
     }
 
+    private static int pow2mod(int exp) {
+        long res = 1, base = 2;
+        while (exp > 0) {
+            if ((exp & 1) != 0) res = (res * base) % MOD;
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return (int) res;
+    }
+
     public int[] productQueries(int n, int[][] queries) {
         List<Integer> lst = new ArrayList<>();
         int idx = find_biggest(n), sum = 0;
         for (int i = idx; i >= 0; i--) {
             if (sum + arr[i] <= n) {
-                lst.add(arr[i]);
+                lst.add(i);  
                 sum += arr[i];
             }
         }
 
         int m = lst.size();
         int[] prefix = new int[m];
-        int[] invPrefix = new int[m];
-
-        prefix[0] = lst.get(0) % MOD;
+        prefix[0] = lst.get(0);
         for (int i = 1; i < m; i++) {
-            prefix[i] = (int) (((long) prefix[i - 1] * lst.get(i)) % MOD);
-        }
-
-        for (int i = 0; i < m; i++) {
-            invPrefix[i] = (int) modInverse(prefix[i]);
+            prefix[i] = prefix[i - 1] + lst.get(i);
         }
 
         int[] res = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
             int left = m - 1 - queries[i][1];
             int right = m - 1 - queries[i][0];
-            if (left == 0) {
-                res[i] = prefix[right];
-            } else {
-                res[i] = (int) ((long) prefix[right] * invPrefix[left - 1] % MOD);
-            }
-        }
-        return res;
-    }
-
-    private static long modInverse(long a) {
-        return modPow(a, MOD - 2);
-    }
-
-    private static long modPow(long a, long b) {
-        long res = 1;
-        while (b > 0) {
-            if ((b & 1) != 0) res = (res * a) % MOD;
-            a = (a * a) % MOD;
-            b >>= 1;
+            int expSum = prefix[right] - (left > 0 ? prefix[left - 1] : 0);
+            res[i] = pow2mod(expSum);
         }
         return res;
     }
