d09976049b32a65c149d06cbe9c7933aa7c65893
Manav Desai <dmanavpianist@gmail.com>
2025-08-11 13:18:05 +0530

    Time: 17 ms (83.87%) | Memory: 75.3 MB (69.89%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -2,26 +2,6 @@ import java.util.*;
 
 class Solution {
     static final int MOD = (int) 1e9 + 7;
-    static int[] arr = new int[30];
-    static {
-        for (int i = 0; i < 30; i++) {
-            arr[i] = 1 << i;
-        }
-    }
-
-    static int find_biggest(int n) {
-        int low = 0, high = arr.length - 1, ans = -1;
-        while (low <= high) {
-            int mid = (low + high) >>> 1;
-            if (arr[mid] <= n) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return ans;
-    }
 
     private static int pow2mod(int exp) {
         long res = 1, base = 2;
@@ -35,26 +15,30 @@ class Solution {
 
     public int[] productQueries(int n, int[][] queries) {
         List<Integer> lst = new ArrayList<>();
-        int idx = find_biggest(n), sum = 0;
-        for (int i = idx; i >= 0; i--) {
-            if (sum + arr[i] <= n) {
-                lst.add(i);  
-                sum += arr[i];
-            }
+        int pow = 0;
+        int tmp = n;
+        while (tmp > 0) {
+            if ((tmp & 1) != 0) lst.add(pow); // store exponent (0,1,2,...), ascending order
+            pow++;
+            tmp >>= 1;
         }
 
         int m = lst.size();
+        if (m == 0) {
+            int[] resEmpty = new int[queries.length];
+            Arrays.fill(resEmpty, 0);
+            return resEmpty;
+        }
+
         int[] prefix = new int[m];
         prefix[0] = lst.get(0);
-        for (int i = 1; i < m; i++) {
-            prefix[i] = prefix[i - 1] + lst.get(i);
-        }
+        for (int i = 1; i < m; i++) prefix[i] = prefix[i - 1] + lst.get(i);
 
         int[] res = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            int left = m - 1 - queries[i][1];
-            int right = m - 1 - queries[i][0];
-            int expSum = prefix[right] - (left > 0 ? prefix[left - 1] : 0);
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int expSum = prefix[r] - (l > 0 ? prefix[l - 1] : 0);
             res[i] = pow2mod(expSum);
         }
         return res;
