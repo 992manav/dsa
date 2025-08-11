9b7e02a669aae07f1992b5589609144630793eb2
Manav Desai <dmanavpianist@gmail.com>
2025-08-11 13:30:55 +0530

    Time: 41 ms (32.25%) | Memory: 45.7 MB (79.98%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -1,15 +1,17 @@
 class Solution {
     public String makeFancyString(String s) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && sb.length() > 0 &&
+        int n = s.length();
 
-                sb.charAt(sb.length() - 1) == s.charAt(i) &&
-                
-                s.charAt(i) == s.charAt(i + 1)) {
+        for (int i = 0; i < n; i++) {
+            char curr = s.charAt(i);
+            if (i < n - 1 &&
+                sb.length() > 0 &&
+                sb.charAt(sb.length() - 1) == curr &&
+                curr == s.charAt(i + 1)) {
                 continue;
             }
-            sb.append(s.charAt(i));
+            sb.append(curr);
         }
         return sb.toString();
     }
