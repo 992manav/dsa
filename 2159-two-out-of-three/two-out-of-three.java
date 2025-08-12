c12495ed670b4b5a24ca254e5c4d724de51f6cf9
Manav Desai <dmanavpianist@gmail.com>
2025-08-12 21:42:19 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (74.79%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.java
+++ 2159-two-out-of-three/two-out-of-three.java
@@ -7,23 +7,23 @@ class Solution {
         Arrays.fill(arr, 0); 
          
         for (int i = 0; i < nums1.length; i++) { 
-            arr[nums1[i]] = 1; 
+            if (arr[nums1[i]] != 1) {
+                arr[nums1[i]] += 1; 
+            }
         } 
         for (int i = 0; i < nums2.length; i++) { 
-            if (arr[nums2[i]] == 1 || arr[nums2[i]] == -1) { 
-                arr[nums2[i]] = -1; // Appeared in nums1 and nums2
-            } else { 
-                arr[nums2[i]] = 2; 
+            if (arr[nums2[i]] == 1 || arr[nums2[i]] == 0) { 
+                arr[nums2[i]] += 2;
             } 
         } 
         for (int i = 0; i < nums3.length; i++) { 
-            if (arr[nums3[i]] != 0) { 
-                arr[nums3[i]] = 3; // Appeared in nums3 + (nums1 or nums2)
+            if (arr[nums3[i]] > 0 && arr[nums3[i]] <= 3) { 
+                arr[nums3[i]] += 3; // fixed from =+ to +=
             } 
         } 
  
         for (int i = 0; i < arr.length; i++) { 
-            if (arr[i] == -1 || arr[i] == 3) { 
+            if (arr[i] >= 3) { 
                 lst.add(i); 
             } 
         } 
