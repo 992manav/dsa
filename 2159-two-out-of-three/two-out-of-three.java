4c718b4a86a2829bbb5914e391f0cb4ad6777aa2
Manav Desai <dmanavpianist@gmail.com>
2025-08-12 21:45:05 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (74.79%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.java
+++ 2159-two-out-of-three/two-out-of-three.java
@@ -1,33 +1,30 @@
 import java.util.*;
 
-class Solution { 
-    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) { 
-        int[] arr = new int[101]; // To store state of each number (1..100)
-        List<Integer> lst = new ArrayList<>(); 
-        Arrays.fill(arr, 0); 
-         
-        for (int i = 0; i < nums1.length; i++) { 
-            if (arr[nums1[i]] != 1) {
-                arr[nums1[i]] += 1; 
+class Solution {
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        int[] mask = new int[101]; // store bitmask for each number
+        List<Integer> result = new ArrayList<>();
+
+        // Mark presence in nums1 (bit 0)
+        for (int num : nums1) {
+            mask[num] |= 1; // set bit 0
+        }
+        // Mark presence in nums2 (bit 1)
+        for (int num : nums2) {
+            mask[num] |= 2; // set bit 1
+        }
+        // Mark presence in nums3 (bit 2)
+        for (int num : nums3) {
+            mask[num] |= 4; // set bit 2
+        }
+
+        // Collect numbers that appear in at least 2 arrays
+        for (int i = 1; i <= 100; i++) {
+            // Count bits set in mask[i]
+            if (Integer.bitCount(mask[i]) >= 2) {
+                result.add(i);
             }
-        } 
-        for (int i = 0; i < nums2.length; i++) { 
-            if (arr[nums2[i]] == 1 || arr[nums2[i]] == 0) { 
-                arr[nums2[i]] += 2;
-            } 
-        } 
-        for (int i = 0; i < nums3.length; i++) { 
-            if (arr[nums3[i]] > 0 && arr[nums3[i]] <= 3) { 
-                arr[nums3[i]] += 3; // fixed from =+ to +=
-            } 
-        } 
- 
-        for (int i = 0; i < arr.length; i++) { 
-            if (arr[i] >= 3) { 
-                lst.add(i); 
-            } 
-        } 
- 
-        return lst; 
-    } 
+        }
+        return result;
+    }
 }
