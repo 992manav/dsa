ea86269c8144958f478dabe27dcde5ac61251a51
Manav Desai <dmanavpianist@gmail.com>
2025-08-13 11:00:19 +0530

    Time: 9 ms (10.87%) | Memory: 44.4 MB (28.08%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,16 +1,10 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n <= 0) {
-            return false;
-        }
+        if (n <= 0) return false;
 
         double p = Math.log(n) / Math.log(3);
         int pInt = (int) Math.round(p);
 
-        if (Math.pow(3, pInt) == n) {
-            return true;
-        }
-
-        return false;
+        return Math.pow(3, pInt) == n;
     }
 }
