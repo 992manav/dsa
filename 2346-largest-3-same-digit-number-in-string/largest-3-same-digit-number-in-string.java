88048b25cebe8afdf9d537b94498b5055b9ca0a7
Manav Desai <dmanavpianist@gmail.com>
2025-08-14 12:58:15 +0530

    Time: 7 ms (27.54%) | Memory: 42.7 MB (31.82%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -3,16 +3,7 @@ class Solution {
         char ans = 0;
         for (int i = 0; i <= num.length() - 3; i++) {
             char c = num.charAt(i);
-            int count = 0;
-            for (int j = 1; j < 3; j++) {
-                if (num.charAt(i + j) == c) {
-                    count++;
-                } else {
-                    break;
-                }
-            }
-
-            if (count == 2) {
+            if (c == num.charAt(i + 1) && c == num.charAt(i + 2)) {
                 ans = (c > ans) ? c : ans;
             }
         }
