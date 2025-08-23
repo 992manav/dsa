90a2df4a7694829d21e20c0a56fec43240954997
Manav Desai <dmanavpianist@gmail.com>
2025-08-23 16:24:24 +0530

    Time: 52 ms (20.00%) | Memory: 82.7 MB (44.44%) - LeetSync

---
+++ 2700-substring-xor-queries/substring-xor-queries.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+class Solution {
+
+    int find(int num, Map<Integer, Integer> map) {
+        return map.getOrDefault(num, -1);
+    }
+
+    int calc(int num, int r) {
+        int len = 0;
+        while (num != 0) {
+            num >>= 1;
+            len++;
+        }
+        return r - len + 1;
+    }
+
+    public int[][] substringXorQueries(String s, int[][] queries) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int first_zero = -1;
+        int first_one = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0' && first_zero == -1) {
+                first_zero = i;
+            }
+            if (s.charAt(i) == '1' && first_one == -1) {
+                first_one = i;
+            }
+
+            if (s.charAt(i) == '0') {
+                if (!map.containsKey(0)) map.put(0, i);
+                continue;
+            }
+            int num = 0;
+            for (int j = i; j < s.length() && j < i + 32; j++) {
+                num = (num << 1);
+                if (s.charAt(j) == '1') {
+                    num += 1;
+                }
+                if (!map.containsKey(num)) {
+                    map.put(num, j); // store right index
+                }
+            }
+        }
+
+        int[][] ans = new int[queries.length][2];
+        Map<Integer, int[]> xorAns = new HashMap<>();
+
+        for (int i = 0; i < queries.length; i++) {
+            int xor = queries[i][0] ^ queries[i][1];
+
+            if (xor == 0) {
+                ans[i] = (first_zero == -1) ? new int[]{-1, -1} : new int[]{first_zero, first_zero};
+                continue;
+            }
+            if (xor == 1) {
+                ans[i] = (first_one == -1) ? new int[]{-1, -1} : new int[]{first_one, first_one};
+                continue;
+            }
+
+            if (xorAns.containsKey(xor)) {
+                ans[i] = xorAns.get(xor);
+                continue;
+            }
+
+            int r = find(xor, map);
+            if (r == -1) {
+                ans[i] = new int[]{-1, -1};
+            } else {
+                int l = calc(xor, r);
+                ans[i] = new int[]{l, r};
+            }
+
+            xorAns.put(xor, ans[i]);
+        }
+        return ans;
+    }
+}
