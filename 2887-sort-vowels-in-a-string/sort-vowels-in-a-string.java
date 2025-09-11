6eb36054130de62ec1c75c06cb8dfac71e9bbce0
Manav Desai <dmanavpianist@gmail.com>
2025-09-11 19:05:29 +0530

    Time: 44 ms (70.95%) | Memory: 49.7 MB (10.72%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -2,37 +2,31 @@ import java.util.*;
 
 class Solution {
     public String sortVowels(String s) {
-        List<Integer> lst = new ArrayList<>();
+        List<Integer> positions = new ArrayList<>();
         List<Character> vowels = new ArrayList<>();
 
-        char[] res = new char[s.length()];
+        char[] res = s.toCharArray();
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (check(c)) {
-                lst.add(i);
-                vowels.add(c);
+        // collect vowels and their positions
+        for (int i = 0; i < res.length; i++) {
+            if (isVowel(res[i])) {
+                positions.add(i);
+                vowels.add(res[i]);
             }
-            res[i] = s.charAt(i);
         }
 
-        char[] arr = new char[vowels.size()];
-        for (int i = 0; i < vowels.size(); i++) {
-            arr[i] = vowels.get(i);
-        }
+        // sort vowels
+        Collections.sort(vowels);
 
-        Arrays.sort(arr);
-        int k = 0;
-        for (int i = 0; i < lst.size(); i++) {
-            int index = lst.get(i);
-            res[index] = arr[k];
-            k++;
+        // put back sorted vowels
+        for (int i = 0; i < positions.size(); i++) {
+            res[positions.get(i)] = vowels.get(i);
         }
 
         return new String(res);
     }
 
-    private boolean check(char c) {
+    private boolean isVowel(char c) {
         return "aeiouAEIOU".indexOf(c) != -1;
     }
 }
