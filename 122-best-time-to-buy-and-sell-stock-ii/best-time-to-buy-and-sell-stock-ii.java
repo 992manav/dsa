664ee558155bcc24058c5c55f6ae2c620787f705
Manav Desai <dmanavpianist@gmail.com>
2025-09-13 12:04:55 +0530

    Time: 3 ms (11.53%) | Memory: 46.2 MB (19.71%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] nextGreater = new int[n];
+        Arrays.fill(nextGreater, -1);
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            boolean fall = false; // reset for each i
+
+            // pop until we find a bigger element
+            while (!st.isEmpty() && prices[st.peek()] <= prices[i]) {
+                fall = true;
+                st.pop();
+            }
+
+            // only assign nextGreater if no fall happened
+            if (!st.isEmpty() && !fall) {
+                nextGreater[i] = st.peek();
+            }
+
+            st.push(i);
+        }
+
+        int profit = 0;
+        for (int i = 0; i < n; i++) {
+            if (nextGreater[i] != -1) {
+                profit += prices[nextGreater[i]] - prices[i];
+            }
+        }
+
+        return profit;
+    }
+}
