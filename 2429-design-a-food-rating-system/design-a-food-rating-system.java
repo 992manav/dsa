87c67f251244563e8bf1ce62e904690969e53812
Manav Desai <dmanavpianist@gmail.com>
2025-09-17 13:08:45 +0530

    Time: 220 ms (34.54%) | Memory: 91.9 MB (5.15%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.java
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.java
@@ -8,35 +8,33 @@ class FoodRatings {
 
     public void insert(String f, String c, int r) {
         map.computeIfAbsent(c, k -> new TreeMap<>())
-            .computeIfAbsent(r, k -> new TreeSet<>())
-            .add(f);
+           .computeIfAbsent(r, k -> new TreeSet<>())
+           .add(f);
     }
 
     public FoodRatings(String[] foods, String[] cus, int[] rat) {
         n = foods.length;
         for (int i = 0; i < n; i++) {
-            String f = foods[i];
-            String c = cus[i];
-            int r = rat[i];
-            cu_f_map.put(f, c);
-            f_r_map.put(f, r);
-            insert(f, c, r);
+            cu_f_map.put(foods[i], cus[i]);
+            f_r_map.put(foods[i], rat[i]);
+            insert(foods[i], cus[i], rat[i]);
         }
     }
 
     public void changeRating(String f, int nr) {
         String c = cu_f_map.get(f);
-        int r = f_r_map.get(f);
-        TreeMap<Integer, TreeSet<String>> mp = map.get(c);
-        TreeSet<String> set = mp.get(r);
+        int oldR = f_r_map.get(f);
+
+        TreeSet<String> set = map.get(c).get(oldR);
         set.remove(f);
-        if (set.isEmpty()) mp.remove(r);
+        if (set.isEmpty()) map.get(c).remove(oldR);
+
         insert(f, c, nr);
         f_r_map.put(f, nr);
     }
 
     public String highestRated(String c) {
-        TreeMap<Integer, TreeSet<String>> mp = map.get(c);
-        return mp.get(mp.lastKey()).first();
+        Map.Entry<Integer, TreeSet<String>> entry = map.get(c).lastEntry();
+        return entry.getValue().first();
     }
 }
