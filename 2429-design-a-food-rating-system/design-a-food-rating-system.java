0a51fcec5275b8595d5ce4a7214ffe4c4e6c9a10
Manav Desai <dmanavpianist@gmail.com>
2025-09-17 13:06:26 +0530

    Time: 238 ms (26.06%) | Memory: 78.8 MB (15.45%) - LeetSync

---
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class FoodRatings {
+    HashMap<String, TreeMap<Integer, TreeSet<String>>> map = new HashMap<>();
+    HashMap<String, String> cu_f_map = new HashMap<>();
+    HashMap<String, Integer> f_r_map = new HashMap<>();
+    int n;
+
+    public void insert(String f, String c, int r) {
+        if (map.containsKey(c)) {
+            TreeMap<Integer, TreeSet<String>> mp = map.get(c);
+            if (mp.containsKey(r)) {
+                TreeSet<String> set = mp.get(r);
+                set.add(f);
+            } else {
+                TreeSet<String> set = new TreeSet<>();
+                set.add(f);
+                mp.put(r, set);
+            }
+        } else {
+            TreeMap<Integer, TreeSet<String>> mp = new TreeMap<>();
+            TreeSet<String> set = new TreeSet<>();
+            set.add(f);
+            mp.put(r, set);
+            map.put(c, mp);
+        }
+    }
+
+    public FoodRatings(String[] foods, String[] cus, int[] rat) {
+        n = foods.length;
+        for (int i = 0; i < n; i++) {
+            String f = foods[i];
+            String c = cus[i];
+            int r = rat[i];
+            cu_f_map.put(f, c);
+            f_r_map.put(f, r);
+            insert(f, c, r);
+        }
+    }
+
+    public void changeRating(String f, int nr) {
+        String c = cu_f_map.get(f);
+        int r = f_r_map.get(f);
+        TreeMap<Integer, TreeSet<String>> mp = map.get(c);
+        TreeSet<String> set = mp.get(r);
+        set.remove(f);
+        if (set.isEmpty()) {
+            mp.remove(r);
+        }
+        insert(f, c, nr);
+        f_r_map.put(f, nr);
+    }
+
+    public String highestRated(String c) {
+        TreeMap<Integer, TreeSet<String>> mp = map.get(c);
+        int highestRating = mp.lastKey();
+        TreeSet<String> set = mp.get(highestRating);
+        return set.first();
+    }
+}
