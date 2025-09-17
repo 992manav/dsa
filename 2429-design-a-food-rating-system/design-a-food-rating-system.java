38cf108cab10f9c114031ddb3ebb83a478626d6c
Manav Desai <dmanavpianist@gmail.com>
2025-09-17 13:11:09 +0530

    Time: 223 ms (32.42%) | Memory: 91.9 MB (5.15%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.java
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 class FoodRatings {
-    HashMap<String, TreeMap<Integer, TreeSet<String>>> map = new HashMap<>();
-    HashMap<String, String> cu_f_map = new HashMap<>();
-    HashMap<String, Integer> f_r_map = new HashMap<>();
-    int n;
+    private final Map<String, TreeMap<Integer, TreeSet<String>>> map = new HashMap<>();
+    private final Map<String, String> cu_f_map = new HashMap<>();
+    private final Map<String, Integer> f_r_map = new HashMap<>();
 
     public void insert(String f, String c, int r) {
         map.computeIfAbsent(c, k -> new TreeMap<>())
@@ -13,8 +12,7 @@ class FoodRatings {
     }
 
     public FoodRatings(String[] foods, String[] cus, int[] rat) {
-        n = foods.length;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < foods.length; i++) {
             cu_f_map.put(foods[i], cus[i]);
             f_r_map.put(foods[i], rat[i]);
             insert(foods[i], cus[i], rat[i]);
@@ -24,17 +22,17 @@ class FoodRatings {
     public void changeRating(String f, int nr) {
         String c = cu_f_map.get(f);
         int oldR = f_r_map.get(f);
+        TreeMap<Integer, TreeSet<String>> mp = map.get(c);
 
-        TreeSet<String> set = map.get(c).get(oldR);
-        set.remove(f);
-        if (set.isEmpty()) map.get(c).remove(oldR);
+        TreeSet<String> oldSet = mp.get(oldR);
+        oldSet.remove(f);
+        if (oldSet.isEmpty()) mp.remove(oldR);
 
-        insert(f, c, nr);
         f_r_map.put(f, nr);
+        mp.computeIfAbsent(nr, k -> new TreeSet<>()).add(f);
     }
 
     public String highestRated(String c) {
-        Map.Entry<Integer, TreeSet<String>> entry = map.get(c).lastEntry();
-        return entry.getValue().first();
+        return map.get(c).lastEntry().getValue().first();
     }
 }
