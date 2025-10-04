cb9ad33cc801d9d1ce91006f0ef78c7c352b16d0
Manav Desai <dmanavpianist@gmail.com>
2025-10-04 15:12:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (51.65%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,54 +1,39 @@
 class Solution {
 
-    int m, n;
-    int[][] grid;
-    int[][] dp;
-
-    int fun(int i, int j) {
-
-        // Out of bounds
-        if (i >= m || j >= n) {
-            return 0;
-        }
-
-        // Blocked cell
-        if (grid[i][j] == 1) {
-            return 0;
-        }
-
-        // Destination reached
-        if (i == m - 1 && j == n - 1) {
-            return 1;
-        }
-
-        // Check memo
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        // Explore down and right
-        int down = fun(i + 1, j);
-        int right = fun(i, j + 1);
-
-        return dp[i][j] = down + right;
-    }
-
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        this.grid = obstacleGrid;
-        this.m = grid.length;
-        this.n = grid[0].length;
+       
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
 
         // If start is blocked, no path
-        if (grid[0][0] == 1) return 0;
+        if (obstacleGrid[0][0] == 1) return 0;
 
-        // Initialize dp array with -1
-        dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = -1;
+        int[][] dp = new int[m][n];
+
+        dp[m-1][n-1] = (obstacleGrid[m-1][n-1] == 0) ? 1 : 0;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                // Blocked cell
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                // Destination already initialized
+                if (i == m - 1 && j == n - 1) {
+                    continue;
+                }
+
+                // Explore down and right
+                int down = (i + 1 < m) ? dp[i + 1][j] : 0;
+                int right = (j + 1 < n) ? dp[i][j + 1] : 0;
+
+                dp[i][j] = down + right;
             }
         }
 
-        return fun(0, 0);
+        return dp[0][0];
     }
 }
