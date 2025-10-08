a2e0cca29bd73a7248a3dcaf0f4c7927b6b255e6
Manav Desai <dmanavpianist@gmail.com>
2025-10-08 21:42:48 +0530

    Time: 5 ms (99.58%) | Memory: 42.1 MB (63.77%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,54 +1,27 @@
 class Solution {
-
     int[] arr;
     int k;
-    int[][] dp;
+    int[] dp;
 
-    public int fun(int noElePrevPartition, int index, int prevPartitionMax, int sum) {
-        if (index >= arr.length) {
-            return sum;
+    public int fun(int index) {
+        if (index >= arr.length) return 0;
+        if (dp[index] != -1) return dp[index];
+
+        int max = 0, res = 0;
+        for (int i = 0; i < k && index + i < arr.length; i++) {
+            max = Math.max(max, arr[index + i]);
+            res = Math.max(res, max * (i + 1) + fun(index + i + 1));
         }
 
-        if (dp[index][noElePrevPartition] != -1) {
-            // dp stores the best result from this state onwards, not total sum
-            return dp[index][noElePrevPartition] + sum;
-        }
-
-        int combine = 0;
-        int notCombine = 0;
-        int currEle = arr[index];
-
-        if (noElePrevPartition < k) {
-            int max = prevPartitionMax;
-            int newSum = sum;
-
-            if (currEle > prevPartitionMax) {
-                max = currEle;
-                newSum = sum - (prevPartitionMax * noElePrevPartition)
-                        + ((noElePrevPartition + 1) * currEle);
-            } else {
-                newSum += prevPartitionMax;
-            }
-
-            combine = fun(noElePrevPartition + 1, index + 1, max, newSum);
-        }
-
-        notCombine = fun(1, index + 1, currEle, sum + currEle);
-
-        int ans = Math.max(combine, notCombine);
-        dp[index][noElePrevPartition] = ans - sum; // store only the remaining part
-        return ans;
+        dp[index] = res;
+        return res;
     }
 
     public int maxSumAfterPartitioning(int[] arr, int k) {
         this.arr = arr;
         this.k = k;
-        this.dp = new int[arr.length + 1][k + 1];
-        for (int i = 0; i <= arr.length; i++) {
-            for (int j = 0; j <= k; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        return fun(0, 0, 0, 0);
+        this.dp = new int[arr.length];
+        Arrays.fill(dp, -1);
+        return fun(0);
     }
 }
