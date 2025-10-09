cf5e18720893301c3f7bc7796a7c1feacc98149b
Manav Desai <dmanavpianist@gmail.com>
2025-10-10 01:31:06 +0530

    Time: 217 ms (16.67%) | Memory: 44.6 MB (77.78%) - LeetSync

---
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+
+    int n;
+    int[] arr, brr;
+
+    void construct(int ind, long[] future) {
+        for (int i = 0; i < n; i++) {
+            if (i == 0) future[i] = (long) brr[ind] * arr[i];
+            else future[i] = (long) brr[ind] * arr[i] + future[i - 1];
+        }
+    }
+
+    long findMaxDifference(long[] current, long[] future) {
+        long max = Long.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            long prevFuture = (i == 0 ? 0 : future[i - 1]);
+            long diff = current[i] - prevFuture;
+            max = Math.max(max, diff);
+        }
+        return max;
+    }
+
+    long minTime(int[] arr, int[] brr) {
+        this.arr = arr;
+        this.brr = brr;
+        n = arr.length;
+        int m = brr.length;
+
+        long[] current = new long[n];
+        long[] future = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) current[i] = (long) brr[0] * arr[i];
+            else current[i] = (long) brr[0] * arr[i] + current[i - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            construct(i, future);
+            long diff = findMaxDifference(current, future);
+            for (int j = 0; j < n; j++) current[j] = future[j] + diff;
+        }
+
+        return current[n - 1];
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {3, 5, 3, 9};
+        int[] brr = {1, 10, 7, 3};
+        System.out.println(sol.minTime(arr, brr));
+    }
+}
