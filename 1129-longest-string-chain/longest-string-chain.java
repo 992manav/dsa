edbd887405f3f1b4de880a36e398100ebd9096f2
Manav Desai <dmanavpianist@gmail.com>
2025-10-09 19:02:31 +0530

    Time: 23 ms (98.57%) | Memory: 45.1 MB (28.81%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -6,43 +6,50 @@ class Solution {
     List<Integer>[] graph;
 
     int dfs(int node, boolean[] visited) {
-        if (!visited[node]) {
-            visited[node] = true;
-            if (dp[node] != -1) return dp[node];
-            int max = 0;
-            for (int next : graph[node]) {
-                if (!visited[next]) max = Math.max(max, dfs(next, visited));
-            }
-            dp[node] = 1 + max;
+        if (dp[node] != -1) return dp[node];
+        visited[node] = true;
+        int max = 0;
+        for (int next : graph[node]) {
+            if (!visited[next]) max = Math.max(max, dfs(next, visited));
         }
-        return dp[node];
+        visited[node] = false;
+        return dp[node] = 1 + max;
     }
 
     public int longestStrChain(String[] words) {
         int n = words.length;
         dp = new int[n];
         Arrays.fill(dp, -1);
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        Arrays.sort(words, Comparator.comparingInt(String::length));
         graph = new ArrayList[n];
         for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        Map<Integer, List<Integer>> byLen = new HashMap<>();
         for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (check(words[i], words[j])) graph[i].add(j);
+            byLen.computeIfAbsent(words[i].length(), k -> new ArrayList<>()).add(i);
+        for (int len : byLen.keySet()) {
+            if (!byLen.containsKey(len + 1)) continue;
+            for (int i : byLen.get(len)) {
+                for (int j : byLen.get(len + 1)) {
+                    if (check(words[i], words[j])) graph[i].add(j);
+                }
+            }
+        }
         int maxChain = 0;
         boolean[] visited = new boolean[n];
-        for (int i = 0; i < n; i++) maxChain = Math.max(maxChain, dfs(i, visited));
+        for (int i = 0; i < n; i++)
+            maxChain = Math.max(maxChain, dfs(i, visited));
         return maxChain;
     }
 
     boolean check(String a, String b) {
-        if (b.length() != a.length() + 1) return false;
-        int i = 0, j = 0;
-        boolean skipped = false;
-        while (i < a.length() && j < b.length()) {
-            if (a.charAt(i) == b.charAt(j)) { i++; j++; }
-            else { if (skipped) return false; skipped = true; j++; }
+        int lenA = a.length(), lenB = b.length();
+        if (lenB != lenA + 1) return false;
+        int i = 0, j = 0, diff = 0;
+        while (i < lenA && j < lenB) {
+            if (a.charAt(i) == b.charAt(j)) i++;
+            else if (++diff > 1) return false;
+            j++;
         }
         return true;
     }
-
 }
