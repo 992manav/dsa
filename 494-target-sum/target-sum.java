d3aa1137f78656e23257e8dc0aa6193be39500cf
Manav Desai <dmanavpianist@gmail.com>
2025-10-11 19:28:31 +0530

    Time: 8 ms (62.06%) | Memory: 45.2 MB (19.48%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -3,22 +3,37 @@ class Solution {
     int len;
     int target;
     int[] nums;
-
-    int fun(int i, int sum) {
+    int[][] dp;
+    
+    int fun(int i, int sum, int total) {
         if (i >= len) {
             return sum == target ? 1 : 0;
         }
 
-        int plus = fun(i + 1, sum + nums[i]);
-        int sub = fun(i + 1, sum - nums[i]);
+        if (dp[i][sum + total] != -1) {  
+            return dp[i][sum + total];
+        }
 
-        return plus + sub;
+        int plus = fun(i + 1, sum + nums[i], total);
+        int sub = fun(i + 1, sum - nums[i], total);
+
+        dp[i][sum + total] = plus + sub;
+        return dp[i][sum + total];
     }
 
     public int findTargetSumWays(int[] nums, int target) {
         this.nums = nums;
         this.target = target;
         this.len = nums.length;
-        return fun(0, 0);
+        
+      
+        int total = 0;
+        for (int num : nums) total += num;
+        
+    
+        dp = new int[len][2 * total + 1];
+        for (int[] row : dp) Arrays.fill(row, -1);
+        
+        return fun(0, 0, total);
     }
 }
