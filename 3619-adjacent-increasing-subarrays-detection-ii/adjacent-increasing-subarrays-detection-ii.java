5b56f405cfb41a6b6cda7c4e46b3762f55e9b952
Manav Desai <dmanavpianist@gmail.com>
2025-10-15 11:55:01 +0530

    Time: 15 ms (74.47%) | Memory: 71.2 MB (87.23%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.java
@@ -4,32 +4,32 @@ class Solution {
     List<Integer> nums;
 
     int check_increasing(int i) {
-        int j = i + 1;
         int n = nums.size();
-        while (j < n && nums.get(j) > nums.get(j - 1)) j++; 
-        return j - i; 
-    } 
- 
-    public int maxIncreasingSubarrays(List<Integer> nums) { 
-        this.nums = nums; 
-        int n = nums.size(), i = 0; 
-        boolean flag = false; 
- 
-        int last_size = 0; 
-        int max = 0; 
-        while (i < n) { 
-            int size = check_increasing(i); 
-            max = Math.max(size / 2, max); 
- 
-            if (last_size <= size) { 
-                max = Math.max(last_size, max); 
-            } else if (size < last_size) {   
-                max = Math.max(size, max); 
-            } 
-            last_size = size; 
-            i += size; 
-        } 
- 
-        return max; 
-    } 
+        int j = i + 1;
+        while (j < n && nums.get(j) > nums.get(j - 1)) j++;
+        return j - i;
+    }
+
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        this.nums = nums;
+        int n = nums.size();
+        int i = 0;
+        int last_size = 0;
+        int max = 0;
+
+        while (i < n) {
+            int size = check_increasing(i);
+
+            if (size / 2 > max) max = size / 2;
+
+            int smaller = size < last_size ? size : last_size;
+
+            if (smaller > max) max = smaller;
+
+            last_size = size;
+            i += size;  
+        }
+
+        return max;
+    }
 }
