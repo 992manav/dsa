cd239c26eb1238fbe8bc6daa02947c0eff7e1038
Manav Desai <dmanavpianist@gmail.com>
2025-11-05 21:45:58 +0530

    Time: 69 ms (43.92%) | Memory: 126.3 MB (5.74%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -2,67 +2,61 @@ import java.util.*;
 
 class Solution {
 
-    int binarySerch_l(int[] nums,int target){
-        int low=0;
-        int high=nums.length-1;
-        int ans=0;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] < target){
-                low = mid+1;
-            }else{
-                ans = mid;
-                high = mid-1;
-            }
-        }
-        return ans;
-    }
-
-    int binarySerch_r(int[] nums,int target){
-        int low=0;
-        int high=nums.length-1;
-        int ans=high;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] <= target){
-                ans = mid;
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
-        }
-        return ans;
-    }
-
     public int maxFrequency(int[] nums, int k, int numOperations) {
-        Arrays.sort(nums);
+
         int n = nums.length;
-        int start=nums[0];
-        int end=nums[n-1];
-        Map<Integer,Integer> map=new HashMap<>();
+
+        int start = Integer.MAX_VALUE;
+        int end = Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            start = Math.min(start, nums[i]);
+            end = Math.max(end, nums[i]);
+        }
+
+        start = start - k;
+        end = end + k;
+
+        int[] freq = new int[end - start + 1];
+
+        int max = 0;
+
+        for(int i=0;i<n;i++){
+            int num = nums[i];
+            int lef = num - k - start;
+            int rig = num + k - start;
+
+            if(lef >= 0 && lef < freq.length) freq[lef] += 1;
+            if(rig + 1 >= 0 && rig + 1 < freq.length) freq[rig + 1] -=1;
+        }
+
+        for(int i=1;i<freq.length;i++){
+            freq[i] = freq[i-1]+freq[i];
+        }
+
+        Map<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<n;i++){
             map.put(nums[i], map.getOrDefault(nums[i],0)+1);
         }
-        int max=0;
-        for(int i=start;i<=end;i++){
-            int target=i;
-            int left=target-k;
-            int right=target+k;
-            int l=binarySerch_l(nums,left);
-            int r=binarySerch_r(nums,right);
-            int target_count=0;
+
+        for(int i=0;i<freq.length;i++){
+            int target = i + start;
+            int target_count = 0;
             if(map.containsKey(target)){
-                target_count =map.get(target);
+                target_count = map.get(target);
             }
-            int available = (r-l+1) - target_count;
+
+            int available = freq[i]-target_count;
+
             if(available > numOperations){
-                int freq=numOperations + target_count;
-                max=Math.max(max,freq);
+                int fr = numOperations + target_count;
+                max = Math.max(max, fr);
             }else{
-                int freq=available + target_count;
-                max=Math.max(max,freq);
+                int fr = available+target_count;
+                max = Math.max(max, fr);
             }
         }
+
         return max;
     }
 }
