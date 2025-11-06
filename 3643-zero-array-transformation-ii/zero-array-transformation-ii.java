17d6b60569de813df977151e8f4d6532df3e9c8e
Manav Desai <dmanavpianist@gmail.com>
2025-11-06 17:38:21 +0530

    Time: 38 ms (10.37%) | Memory: 274 MB (5.40%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,15 +1,16 @@
 class Solution {
     int[] a;
     int[][] q;
+    int[] p;
 
     boolean ok(int k){
         int n=a.length;
-        int[] p=new int[n+1];
+        for(int i=0;i<=n;i++) p[i]=0;
         for(int i=0;i<k;i++){
             int[] x=q[i];
-            int s=x[0],e=x[1],v=x[2];
-            p[s]+=v;
-            if(e+1<n) p[e+1]-=v;
+            p[x[0]]+=x[2];
+            int e=x[1]+1;
+            if(e<n) p[e]-=x[2];
         }
         for(int i=1;i<n;i++) p[i]+=p[i-1];
         for(int i=0;i<n;i++) if(p[i]<a[i]) return false;
@@ -19,6 +20,7 @@ class Solution {
     public int minZeroArray(int[] nums,int[][] queries){
         a=nums;
         q=queries;
+        p=new int[a.length+1];
         int l=0,r=q.length,ans=-1;
         while(l<=r){
             int m=(l+r)>>1;
