56bba0e48dbe14d0ad9eccfde06f06fd2b00c547
Manav Desai <dmanavpianist@gmail.com>
2025-11-06 17:37:32 +0530

    Time: 35 ms (13.69%) | Memory: 231 MB (5.40%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,38 +1,32 @@
 class Solution {
-    int[] nums;
-    int[][] queries;
+    int[] a;
+    int[][] q;
 
-    boolean check(int k){
-        int n=nums.length;
-        int[] prefix=new int[n+1];
+    boolean ok(int k){
+        int n=a.length;
+        int[] p=new int[n+1];
         for(int i=0;i<k;i++){
-            int start=queries[i][0];
-            int end=queries[i][1];
-            int val=queries[i][2];
-            prefix[start]+=val;
-            if(end+1<n) prefix[end+1]-=val;
+            int[] x=q[i];
+            int s=x[0],e=x[1],v=x[2];
+            p[s]+=v;
+            if(e+1<n) p[e+1]-=v;
         }
-        for(int i=1;i<n;i++) prefix[i]+=prefix[i-1];
-        for(int i=0;i<n;i++){
-            if(prefix[i]<nums[i]) return false;
-        }
+        for(int i=1;i<n;i++) p[i]+=p[i-1];
+        for(int i=0;i<n;i++) if(p[i]<a[i]) return false;
         return true;
     }
 
-    public int minZeroArray(int[] nums, int[][] queries) {
-        this.nums=nums;
-        this.queries=queries;
-        int low=0;
-        int high=queries.length;
-        int ans=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(check(mid)){
-                ans=mid;
-                high=mid-1;
-            }else low=mid+1;
+    public int minZeroArray(int[] nums,int[][] queries){
+        a=nums;
+        q=queries;
+        int l=0,r=q.length,ans=-1;
+        while(l<=r){
+            int m=(l+r)>>1;
+            if(ok(m)){
+                ans=m;
+                r=m-1;
+            }else l=m+1;
         }
-        // if(ans==queries.length) return -1;
         return ans;
     }
 }
