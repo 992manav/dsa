b4f5d81a13be96b3b2acf90921729d416e554f20
Manav Desai <dmanavpianist@gmail.com>
2025-11-06 15:39:22 +0530

    Time: 1 ms (18.16%) | Memory: 48.7 MB (5.22%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,30 +1,20 @@
 import java.util.*;
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i = 0;
-        int j = 0;
-        int length = nums.length;
-        while (j < length) {
-            if (j < length - 1 && nums[j] != Integer.MAX_VALUE && nums[j + 1] != Integer.MAX_VALUE && nums[j] == nums[j + 1]) {
-                if (i < length) nums[i] = nums[j];
-                if (i + 1 < length) nums[i + 1] = nums[j + 1];
-                if (i != j) {
-                    if (j > i + 1 && j < length) nums[j] = Integer.MAX_VALUE;  // Only mark if j is beyond i+1
-                    if (j + 1 > i + 1 && j + 1 < length) nums[j + 1] = Integer.MAX_VALUE;  // Only mark if j+1 is beyond i+1
+        int i=0,j=0,n=nums.length;
+        while(j<n){
+            if(j<n-1 && nums[j]!=Integer.MAX_VALUE && nums[j+1]!=Integer.MAX_VALUE && nums[j]==nums[j+1]){
+                nums[i]=nums[j];
+                if(i+1<n) nums[i+1]=nums[j+1];
+                if(i!=j){
+                    if(j>i+1) nums[j]=Integer.MAX_VALUE;
+                    if(j+1>i+1) nums[j+1]=Integer.MAX_VALUE;
                 }
-                i = i + 2;
-                j = j + 2;
-                while (j < length && (nums[j] == nums[i - 1] || nums[j] == Integer.MAX_VALUE)) {
-                    j++;
-                }
+                i+=2; j+=2;
+                while(j<n && (nums[j]==nums[i-1] || nums[j]==Integer.MAX_VALUE)) j++;
             } else {
-                if (nums[j] != Integer.MAX_VALUE) {
-                    nums[i] = nums[j];
-                    i++;
-                }
-                if (i - 1 != j && j > i - 1 && j < length) {  // Only mark if j is beyond where we wrote
-                    nums[j] = Integer.MAX_VALUE;
-                }
+                if(nums[j]!=Integer.MAX_VALUE) nums[i++]=nums[j];
+                if(i-1!=j && j>i-1) nums[j]=Integer.MAX_VALUE;
                 j++;
             }
         }
