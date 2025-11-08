993682c49a74bc3981d7ae64bc892ab83d0f8221
Manav Desai <dmanavpianist@gmail.com>
2025-11-08 17:41:36 +0530

    Time: 48 ms (98.71%) | Memory: 47.5 MB (6.82%) - LeetSync

--- 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.java
+++ 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.java
@@ -3,7 +3,6 @@ class Solution {
         int len = s.length();
         char first = s.charAt(0);
         char lastc = s.charAt(len - 1);
-        String str = "";
         int min_index = -1;
         char min = first;
         int max = -1;
@@ -17,57 +16,39 @@ class Solution {
                     min_index = i;
                     min = c;
                 } else if (c == min) {
-                    String a = new StringBuilder(s.substring(0, min_index + 1)).reverse().toString() + s.substring(min_index + 1);
-                    String b = new StringBuilder(s.substring(0, i + 1)).reverse().toString() + s.substring(i + 1);
-                    int x = a.compareTo(b);
-                    if (x > 0) {
+                    String a = new StringBuilder(s.substring(0, min_index + 1)).reverse().append(s.substring(min_index + 1)).toString();
+                    String b = new StringBuilder(s.substring(0, i + 1)).reverse().append(s.substring(i + 1)).toString();
+                    if (a.compareTo(b) > 0) {
                         min_index = i;
                     }
                 }
             }
 
-            if (c > lastc && max == -1) {
-                max = i;
-            }
+            if (c > lastc && max == -1) max = i;
 
             if (c == lastc) {
-                String last2 = new StringBuilder(s.substring(0, i)).toString() + new StringBuilder(s.substring(i)).reverse().toString();
+                String last2 = new StringBuilder(s.substring(0, i)).append(new StringBuilder(s.substring(i)).reverse()).toString();
 
                 if (max == -1) {
-                    if (equal == null || last2.compareTo(equal) < 0) {
-                        equal = last2;
-                    }
+                    if (equal == null || last2.compareTo(equal) < 0) equal = last2;
                 } else {
-                    String last1 = new StringBuilder(s.substring(0, max)).toString() + new StringBuilder(s.substring(max)).reverse().toString();
-                    int x = last1.compareTo(last2);
-                    if (x > 0) {
-                        equal = last2;
-                    }
+                    String last1 = new StringBuilder(s.substring(0, max)).append(new StringBuilder(s.substring(max)).reverse()).toString();
+                    if (last1.compareTo(last2) > 0) equal = last2;
                 }
             }
         }
 
-        str = new StringBuilder(s.substring(0, min_index + 1)).reverse().toString() + s.substring(min_index + 1);
+        String str = new StringBuilder(s.substring(0, min_index + 1)).reverse().append(s.substring(min_index + 1)).toString();
 
         String last = null;
         if (max != -1) {
-            last = new StringBuilder(s.substring(0, max)).toString() + new StringBuilder(s.substring(max)).reverse().toString();
-            if (equal != null && equal.compareTo(last) < 0) {
-                last = equal;
-            }
+            last = new StringBuilder(s.substring(0, max)).append(new StringBuilder(s.substring(max)).reverse()).toString();
+            if (equal != null && equal.compareTo(last) < 0) last = equal;
         }
 
-        if (equal != null && (last == null || equal.compareTo(last) < 0)) {
-            last = equal;
-        }
+        if (equal != null && (last == null || equal.compareTo(last) < 0)) last = equal;
 
-        String ans;
-        if (last == null || str.compareTo(last) < 0) {
-            ans = str;
-        } else {
-            ans = last;
-        }
-
-        return ans;
+        if (last == null || str.compareTo(last) < 0) return str;
+        return last;
     }
 }
