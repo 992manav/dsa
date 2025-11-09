34d328792c536876e21f06bd393f199e6881f362
Manav Desai <dmanavpianist@gmail.com>
2025-11-10 00:34:26 +0530

    Time: 15 ms (61.32%) | Memory: 86.8 MB (5.35%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -7,6 +7,8 @@ class Solution {
         int count = 0;
 
         int[] prefix = new int[n];
+
+        // step1 : prefix side pe majority element nikal rahe hai
         for(int i=0;i<n;i++){
             if(count==0){
                 maj1 = nums.get(i);
@@ -26,6 +28,8 @@ class Solution {
         int maj2 = 0;
         count = 0;
         int[] suffix = new int[n];
+
+        // step2 : suffix side pe majority element nikal rahe hai
         for(int i=n-1;i>=0;i--){
             if(count==0){
                 maj2 = nums.get(i);
@@ -46,6 +50,9 @@ class Solution {
         int[] preCnt = new int[n];
         int[] sufCnt = new int[n];
 
+        // step3 : jo element left partition mein majority hoga
+        // woh right partition mein bhi majority hoga
+        // matlab woh element pure array mein majority hoga
         int c = 0;
         for(int i=0;i<n;i++){
             if(nums.get(i)==x){
@@ -62,8 +69,11 @@ class Solution {
             sufCnt[i] = c;
         }
 
+        // step4 : ab check kar rahe ki left aur right dono side pe majority satisfy hota hai ya nahi
         for(int i=0;i<n-1;i++){
             if(prefix[i]==x && suffix[i+1]==x){
+                // left part ka majority count > left part ka size/2
+                // right part ka majority count > right part ka size/2
                 if(preCnt[i]*2 > i+1 && sufCnt[i+1]*2 > n-(i+1)){
                     return i;
                 }
