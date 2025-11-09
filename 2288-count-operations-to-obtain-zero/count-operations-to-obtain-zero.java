d4357861857858f940b16eaba24c1eb654f1c89a
Manav Desai <dmanavpianist@gmail.com>
2025-11-09 12:35:35 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (20.29%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -3,11 +3,12 @@ class Solution {
         int count=0;
         while(num1!=0 && num2!=0){
             if(num1>=num2){
-                num1-=num2;
+                count+=num1/num2;
+                num1=num1%num2;
             }else{
-                num2-=num1;
+                count+=num2/num1;
+                num2=num2%num1;
             }
-            count++;
         }
         return count;
     }
