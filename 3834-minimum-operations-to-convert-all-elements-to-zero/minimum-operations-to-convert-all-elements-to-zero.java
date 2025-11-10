da5e70e98893a8d33b4b6aaa7c167b32e04c61c6
Manav Desai <dmanavpianist@gmail.com>
2025-11-10 13:09:14 +0530

    Time: 180 ms (10.56%) | Memory: 126.5 MB (9.16%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -4,35 +4,36 @@ class Solution {
         int n = nums.length;
         int count = 0;
         Stack<Integer> st = new Stack<>();
+
         for(int i=n-1;i>=0;i--){
             int num = nums[i];
 
             if(st.isEmpty()){
                 st.push(i);
                 if(num!=0){
-                        count++;
-                }
-                
-            }else{
-                if(num > nums[st.peek()]){
-                    st.push(i);
                     count++;
-                }else{
-                    boolean flag=false;
-                    if(num==0){
-                        flag=true;
-                    }
-                    while(!st.isEmpty() && num <= nums[st.peek()]){
-                        if(num == nums[st.peek()]){
-                            flag=true;
-                        }
-                        st.pop();
-                    }
-                    st.push(i);
-                    if(!flag){
-                        count++;
-                    }
                 }
+                continue;
+            }
+
+            if(num > nums[st.peek()]){
+                st.push(i);
+                count++;
+                continue;
+            }
+
+            boolean flag = (num==0);
+
+            while(!st.isEmpty() && num <= nums[st.peek()]){
+                if(num == nums[st.peek()]){
+                    flag = true;
+                }
+                st.pop();
+            }
+
+            st.push(i);
+            if(!flag){
+                count++;
             }
         }
         return count;
