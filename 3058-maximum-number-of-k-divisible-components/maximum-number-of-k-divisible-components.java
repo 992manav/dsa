456dc176536e8daa01a0be9ec333ecf30906d9cf
Manav Desai <dmanavpianist@gmail.com>
2025-11-28 17:13:38 +0530

    Time: 27 ms (63.44%) | Memory: 86.3 MB (36.56%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
@@ -3,34 +3,56 @@ import java.util.*;
 class Solution {
 
     int k;
-    int[] values;
+    long[] values;
     ArrayList<Integer>[] graph;
+    int count;
+    int[] indegree;
+    int n;
 
-    long dfs(int node, int parent, int[] ans) {
+    int bfs(Queue<Integer> q) {
+        boolean[] removed = new boolean[n];
 
-        long sum = values[node];
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            removed[node] = true;
 
-        for (int child : graph[node]) {
-            if (child == parent) continue;
+            List<Integer> lst = graph[node];
+            int parent = -1;
 
-            long childSum = dfs(child, node, ans);
-            sum += childSum;
+            for (int i = 0; i < lst.size(); i++) {
+                int neighbour = lst.get(i);
+                if (!removed[neighbour]) parent = neighbour;
+            }
+
+            if (parent == -1) {
+                if (values[node] % k == 0) count++;
+                continue;
+            }
+
+            indegree[parent]--;
+
+            if (values[node] % k == 0) count++;
+            else values[parent] += values[node];
+
+            if (indegree[parent] == 1) q.offer(parent);
         }
 
-        if (sum % k == 0) {
-            ans[0]++;
-            return 0;
-        }
-
-        return sum;
+        return count;
     }
 
-    public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+    public int maxKDivisibleComponents(int n, int[][] edges, int[] vals, int k) {
+
+        if (n == 1) return 1;
 
         this.k = k;
-        this.values = values;
+        this.n = n;
 
+        values = new long[n];
+        for (int i = 0; i < n; i++) values[i] = vals[i];
+
+        indegree = new int[n];
         graph = new ArrayList[n];
+
         for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
 
         for (int i = 0; i < edges.length; i++) {
@@ -38,12 +60,16 @@ class Solution {
             int v = edges[i][1];
             graph[u].add(v);
             graph[v].add(u);
+            indegree[u]++;
+            indegree[v]++;
         }
 
-        int[] ans = new int[1];
+        Queue<Integer> q = new LinkedList<>();
 
-        dfs(0, -1, ans);
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 1) q.offer(i);
+        }
 
-        return ans[0];
+        return bfs(q);
     }
 }
