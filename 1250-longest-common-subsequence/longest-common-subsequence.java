d128c61d231c84b10ed161b3d598ec07ccd82116
Manav Desai <dmanavpianist@gmail.com>
2025-11-30 03:18:17 +0530

    Time: 21 ms (54.43%) | Memory: 53.8 MB (38.66%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,88 +1,23 @@
-import java.util.*;
-
 class Solution {
-
-    String s, t;
-    int[][] dp;
-    Map<Character, List<Integer>> map = new HashMap<>();
-
-    int binary_search(int index, List<Integer> lst) {
-
-        int low = 0;
-        int high = lst.size() - 1;
-
-        int ans = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (lst.get(mid) <= index) {
-                low = mid + 1;
-            } else {
-                ans = mid;
-                high = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
-    int check(char c, int index) {
-
-        List<Integer> lst = map.get(c);
-        if (lst == null) return -1;
-
-        int pos = binary_search(index, lst); 
-        if (pos != -1) {
-            return lst.get(pos);
-        }
-
-        return -1;
-    }
-
-    int fun(int i, int j) {
-        if (i >= s.length()) return 0;
-
-        if (dp[i][j + 1] != -1) {
-            return dp[i][j + 1];
-        }
-
-        char c = s.charAt(i);
-
-        int take = 0;
-        int index = check(c, j);
-        if (index != -1) {
-            take = 1 + fun(i + 1, index);
-        }
-
-        int skip = fun(i + 1, j);
-
-        return dp[i][j + 1] = Math.max(take, skip);
-    }
-
     public int longestCommonSubsequence(String text1, String text2) {
-        if (text1.length() <= text2.length()) {
-        s = text1;
-        t = text2;
-        } else {
-            s = text2;
-            t = text1;
-        }
+        int n1 = text1.length();
+        int n2 = text2.length();
+        int[][] dp = new int[n1 + 2][n2 + 2];
 
-        dp = new int[s.length()][t.length() + 1];
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j <= t.length(); j++) {
-                dp[i][j] = -1;
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                } else {
+                    if (dp[i + 1][j] > dp[i][j + 1]) {
+                        dp[i][j] = dp[i + 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j + 1];
+                    }
+                }
             }
         }
 
-       
-        map.clear();
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            map.putIfAbsent(c, new ArrayList<>());
-            map.get(c).add(i);
-        }
-
-        return fun(0, -1);
+        return dp[0][0];
     }
 }
