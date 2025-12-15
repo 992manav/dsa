f5ee2d5cd5817e4b956b62ba56afa8d05bad6973
Manav Desai <dmanavpianist@gmail.com>
2025-12-16 00:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (23.38%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -6,35 +6,47 @@ class Solution {
         while (i[0] < s.length() && s.charAt(i[0]) != ']') {
 
             if (Character.isDigit(s.charAt(i[0]))) {
-                int num = 0;
-                while (Character.isDigit(s.charAt(i[0]))) {
-                    num = num * 10 + (s.charAt(i[0]) - '0');
-                    i[0]++;
-                }
-
-                String inner = "";
-
-                if (s.charAt(i[0]) == '[') {
-                    i[0]++;
-                    inner = decode(s, i);
-                    i[0]++;
-                }
-
-                for (int k = 0; k < num; k++) {
-                    res.append(inner);
-                }
-
-            } else if (s.charAt(i[0]) == '[') {
+                int num = readNumber(s, i);
+                String inner = readBracketString(s, i);
+                appendRepeated(res, inner, num);
+            } 
+            else if (s.charAt(i[0]) == '[') {
                 i[0]++;
                 res.append(decode(s, i));
                 i[0]++;
-            } else {
+            } 
+            else {
                 res.append(s.charAt(i[0]));
                 i[0]++;
             }
         }
 
         return res.toString();
+    }
+
+    int readNumber(String s, int[] i) {
+        int num = 0;
+        while (i[0] < s.length() && Character.isDigit(s.charAt(i[0]))) {
+            num = num * 10 + (s.charAt(i[0]) - '0');
+            i[0]++;
+        }
+        return num;
+    }
+
+    String readBracketString(String s, int[] i) {
+        if (s.charAt(i[0]) == '[') {
+            i[0]++;
+            String inner = decode(s, i);
+            i[0]++;
+            return inner;
+        }
+        return "";
+    }
+
+    void appendRepeated(StringBuilder res, String str, int times) {
+        for (int k = 0; k < times; k++) {
+            res.append(str);
+        }
     }
 
     public String decodeString(String s) {
