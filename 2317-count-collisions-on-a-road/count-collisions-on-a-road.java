43feb043669a4e83aa69dfd095093bbc8f441026
Manav Desai <dmanavpianist@gmail.com>
2025-12-17 20:16:19 +0530

    Time: 24 ms (14.81%) | Memory: 47.9 MB (17.31%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,5 +1,6 @@
 class Solution {
 
+    // j se aage same character (R ya S) jab tak milta rahe, index aage badhao
     int moveSame(String s, int j, char c) {
         while (j < s.length() && s.charAt(j) == c) {
             j++;
@@ -7,6 +8,7 @@ class Solution {
         return j;
     }
 
+    // j se continuous L kitne hain, sirf count return karta hai
     int countLeft(String s, int j) {
         int count = 0;
         while (j < s.length() && s.charAt(j) == 'L') {
@@ -24,38 +26,61 @@ class Solution {
 
             char c = s.charAt(i);
 
+            // ================= CASE 1: 'R' =================
             if (c == 'R') {
 
+                // continuous R pakad lo (R, RR, RRR ...)
                 int j = i;
                 j = moveSame(s, j, 'R');
-                int count = j - i;
+                int count = j - i;   // kitne R the
 
+                // agar R ke baad kuch hai
                 if (j < s.length()) {
                     char next = s.charAt(j);
 
+                    // ---------- CASE: R...S ----------
+                    // har R, S se takra ke 1 collision karega
                     if (next == 'S') {
                         collision += count;
-                    } 
+                    }
+
+                    // ---------- CASE: R...L ----------
+                    // pehla collision R-L se (2 cars → +1 extra)
+                    // baaki R bhi ruk jaate hain
                     else if (next == 'L') {
                         collision += count + 1;
+
+                        // L ke baad agar S aaye toh skip
                         j++;
+
+                        // LLL jitne honge sab S se takraenge
                         int cnt = countLeft(s, j);
                         collision += cnt;
+
                         j = j + cnt;
                     }
                 }
-                i = j;
 
-            } 
+                i = j;
+            }
+
+            // ================= CASE 2: 'S' =================
             else if (c == 'S') {
 
                 int j = i;
+
+                // continuous S skip
                 j = moveSame(s, j, 'S');
+
+                // S ke baad jitne L aayenge sab collision karenge
                 int count = countLeft(s, j);
                 collision += count;
-                i = j + count;
 
-            } 
+                i = j + count;
+            }
+
+            // ================= CASE 3: 'L' =================
+            // akela L kisi se takrata nahi
             else {
                 i++;
             }
