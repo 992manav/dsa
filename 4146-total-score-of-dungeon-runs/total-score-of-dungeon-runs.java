d399ed1065e30b157a4675f23c547d7a9f0bd0a1
Manav Desai <dmanavpianist@gmail.com>
2025-12-18 13:44:49 +0530

    Time: 24 ms (82.25%) | Memory: 170.5 MB (81.04%) - LeetSync

---
+++ 4146-total-score-of-dungeon-runs/total-score-of-dungeon-runs.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    int binary_search(int[] prefix_d, int hp, int tot_demand_sum, int r, int max_i) {
+        int low = 0;
+        int high = max_i;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int damage;
+            if (mid == 0) {
+                damage = tot_demand_sum;
+            } else {
+                damage = tot_demand_sum - prefix_d[mid - 1];
+            }
+
+            if (hp - damage >= r) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public long totalScore(int hp, int[] d, int[] req) {
+        int n = d.length;
+        long tot = 0;
+
+        int[] prefix_d = new int[n];
+        prefix_d[0] = d[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix_d[i] = prefix_d[i - 1] + d[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int r = req[i];
+            int tot_demand_sum = prefix_d[i];
+
+            if (hp - tot_demand_sum >= r) {
+                tot += i + 1;
+            } else {
+                int index = binary_search(prefix_d, hp, tot_demand_sum, r, i);
+                if (index != -1) {
+                    tot += (i - index + 1);
+                }
+            }
+        }
+
+        return tot;
+    }
+}
