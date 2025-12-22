cffc88dfbd73b27c974d849ec767697eb2ce6451
Manav Desai <dmanavpianist@gmail.com>
2025-12-22 16:55:11 +0530

    Time: 6 ms (100.00%) | Memory: 44.2 MB (47.44%) - LeetSync

--- 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
+++ 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
@@ -5,28 +5,33 @@ class Solution {
         int cols = strs[0].length();
         int rows = strs.length;
 
+
+        if(cols==1){
+            return 0;
+        }
         int[] lis = new int[cols];
         Arrays.fill(lis, 1);
+
+        int max_len = 0;
 
         for (int currCol = 1; currCol < cols; currCol++) {
             for (int prevCol = 0; prevCol < currCol; prevCol++) {
                 if (isNonDecreasing(strs, prevCol, currCol)) {
-                    lis[currCol] = Math.max(lis[currCol], lis[prevCol] + 1);
+                    int len = Math.max(lis[currCol], lis[prevCol] + 1);
+                    lis[currCol] = len;
                 }
             }
+            max_len = Math.max(max_len, lis[currCol]);
         }
 
-        int maxLis = 0;
-        for (int len : lis) {
-            maxLis = Math.max(maxLis, len);
-        }
-
-        return cols - maxLis;
+        return cols - max_len;
     }
 
     private boolean isNonDecreasing(String[] strs, int leftCol, int rightCol) {
         for (String row : strs) {
-            if (row.charAt(leftCol) > row.charAt(rightCol)) {
+            if (row.charAt(leftCol) <= row.charAt(rightCol)) {
+                continue;
+            } else {
                 return false;
             }
         }
