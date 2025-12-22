baea2cb0d338a89a667151ca1398432b87964eae
Manav Desai <dmanavpianist@gmail.com>
2025-12-22 13:11:03 +0530

    Time: 73 ms (24.09%) | Memory: 47.3 MB (20.84%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,22 +1,44 @@
 import java.util.*;
 
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int[] yahaanKhatamLIS = new int[nums.length];
-        Arrays.fill(yahaanKhatamLIS, 1);
 
-        int sabseBadaLength = 0;
+    int binary_search(TreeMap<Integer, TreeSet<Integer>> map, int target) {
 
-        for (int abhiIndex = 0; abhiIndex < nums.length; abhiIndex++) {
-            for (int pichlaIndex = 0; pichlaIndex < abhiIndex; pichlaIndex++) {
-                if (nums[pichlaIndex] < nums[abhiIndex]) {
-                    yahaanKhatamLIS[abhiIndex] =
-                        Math.max(yahaanKhatamLIS[abhiIndex], yahaanKhatamLIS[pichlaIndex] + 1);
-                }
+        for (Integer len : map.keySet()) {
+            TreeSet<Integer> set = map.get(len);
+            if (set.lower(target) != null) {
+                return len + 1;
             }
-            sabseBadaLength = Math.max(sabseBadaLength, yahaanKhatamLIS[abhiIndex]);
         }
 
-        return sabseBadaLength;
+        return 1;
+    }
+
+    public int lengthOfLIS(int[] nums) {
+
+        TreeMap<Integer, TreeSet<Integer>> map =
+            new TreeMap<>(Collections.reverseOrder());
+
+        TreeSet<Integer> s = new TreeSet<>();
+        s.add(nums[0]);
+        map.put(1, s);
+
+        int max_len = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            int len = binary_search(map, nums[i]);
+
+            if (map.containsKey(len)) {
+                map.get(len).add(nums[i]);
+            } else {
+                TreeSet<Integer> set = new TreeSet<>();
+                set.add(nums[i]);
+                map.put(len, set);
+            }
+
+            max_len = Math.max(max_len, len);
+        }
+
+        return max_len;
     }
 }
