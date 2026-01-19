5ef2e3d199c39da77ec9dfd051e1883b3bd89fa8
Manav Desai <dmanavpianist@gmail.com>
2026-01-19 15:53:43 +0530

    Time: 50 ms (2.70%) | Memory: 46.8 MB (10.29%) - LeetSync

---
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] p) {
+        int x=p[0][0];
+        int y=p[0][1];
+
+        int sum=0;
+        for(int i=1;i<p.length;i++){
+            int new_x=p[i][0];
+            int new_y=p[i][1];
+
+            int diff_x=Math.abs(new_x-x);
+            int diff_y=Math.abs(new_y-y);
+
+            System.out.println("hi");
+            System.out.println(diff_x);
+            System.out.println(diff_y);
+            
+
+            if(diff_x<diff_y){
+                sum+=(diff_x)+(diff_y-diff_x);
+            }else{
+                sum+=(diff_y)+(diff_x-diff_y);
+            }
+
+            System.out.println(sum);
+
+            x=new_x;
+            y=new_y;
+
+        }
+
+        return sum;
+    }
+}
