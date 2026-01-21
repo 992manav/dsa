f5caddd8c367dff22d305fd7a9dc11777a5d900f
Manav Desai <dmanavpianist@gmail.com>
2026-01-21 09:58:09 +0530

    Time: 107 ms (24.07%) | Memory: 259.6 MB (22.22%) - LeetSync

--- 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.java
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.java
@@ -1,29 +1,41 @@
 class Solution {
-    int endPos;
-    long[][] dp;
-    int offset = 1000;
-    final int MOD = 1000000007;
 
-    long fun(int i, int k) {
+    int end;
+    int offset = 1000;
+    int MOD = 1000000007;
+    long[][] dp;
+
+    long fun(int pos, int k) {
+
         if (k == 0) {
-            if (i == endPos) return 1;
+            if (pos == end) {
+                return 1;
+            }
             return 0;
         }
-        if (dp[i + offset][k] != -1) return dp[i + offset][k];
-        long front = fun(i + 1, k - 1);
-        long back = fun(i - 1, k - 1);
-        dp[i + offset][k] = (front + back) % MOD;
-        return dp[i + offset][k];
+
+        if (dp[pos + offset][k] != -1) {
+            return dp[pos + offset][k];
+        }
+
+        long a = fun(pos + 1, k - 1);
+        long b = fun(pos - 1, k - 1);
+
+        dp[pos + offset][k] = (a + b) % MOD;
+        return dp[pos + offset][k];
     }
 
     public int numberOfWays(int startPos, int endPos, int k) {
-        this.endPos = endPos;
+
+        end = endPos;
         dp = new long[3001][k + 1];
+
         for (int i = 0; i < 3001; i++) {
             for (int j = 0; j <= k; j++) {
                 dp[i][j] = -1;
             }
         }
+
         return (int)(fun(startPos, k) % MOD);
     }
 }
