009f8cadc91b203c8ad9455d39acfd100cf0b559
Manav Desai <dmanavpianist@gmail.com>
2026-01-21 16:37:05 +0530

    Time: 242 ms (5.21%) | Memory: 47.2 MB (7.57%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -3,45 +3,62 @@ import java.util.*;
 class Solution {
 
     PriorityQueue<Integer> pq;
-    Set<Integer> seen;
+    HashSet<Integer> seen;
+    int n;
 
-    void fun(int p2, int p3, int p5) {
+    void fun(int p2, int p3, int p5, int idx) {
 
-        int val = 1;
+        if (idx == 4) {
 
-        for (int i = 0; i < p2; i++) {
-            if (val > Integer.MAX_VALUE / 2) return;
-            val *= 2;
+            int val = 1;
+
+            for (int i = 0; i < p2; i++) {
+                if (val > Integer.MAX_VALUE / 2) return;
+                val *= 2;
+            }
+
+            for (int i = 0; i < p3; i++) {
+                if (val > Integer.MAX_VALUE / 3) return;
+                val *= 3;
+            }
+
+            for (int i = 0; i < p5; i++) {
+                if (val > Integer.MAX_VALUE / 5) return;
+                val *= 5;
+            }
+
+            if (!seen.contains(val)) {
+                seen.add(val);
+                pq.add(val);
+            }
+
+            return;
         }
 
-        for (int i = 0; i < p3; i++) {
-            if (val > Integer.MAX_VALUE / 3) return;
-            val *= 3;
-        }
-
-        for (int i = 0; i < p5; i++) {
-            if (val > Integer.MAX_VALUE / 5) return;
-            val *= 5;
-        }
-
-        if (!seen.contains(val)) {
-            seen.add(val);
-            pq.add(val);
+        if (idx == 1) {
+            for (int i = 0; i <= 30; i++) {
+                fun(p2 + i, p3, p5, 2);
+            }
+        } 
+        else if (idx == 2) {
+            for (int i = 0; i <= 20; i++) {
+                fun(p2, p3 + i, p5, 3);
+            }
+        } 
+        else {
+            for (int i = 0; i <= 14; i++) {
+                fun(p2, p3, p5 + i, 4);
+            }
         }
     }
 
     public int nthUglyNumber(int n) {
 
+        this.n = n;
         pq = new PriorityQueue<>();
         seen = new HashSet<>();
 
-        for (int i = 0; i <= 30; i++) {
-            for (int j = 0; j <= 20; j++) {
-                for (int k = 0; k <= 14; k++) {
-                    fun(i, j, k);
-                }
-            }
-        }
+        fun(0, 0, 0, 1);
 
         while (n > 1) {
             pq.poll();
