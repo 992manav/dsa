2a4b7d5082320cefdf5425dd664cea328359a5e1
Manav Desai <dmanavpianist@gmail.com>
2026-01-21 17:37:28 +0530

    Time: 2 ms (99.40%) | Memory: 43.3 MB (67.80%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -2,69 +2,35 @@ import java.util.*;
 
 class Solution {
 
-    PriorityQueue<Integer> pq;
-    HashSet<Integer> seen;
-    int n;
-
-    void fun(int p2, int p3, int p5, int idx) {
-
-        if (idx == 4) {
-
-            int val = 1;
-
-            for (int i = 0; i < p2; i++) {
-                if (val > Integer.MAX_VALUE / 2) return;
-                val *= 2;
-            }
-
-            for (int i = 0; i < p3; i++) {
-                if (val > Integer.MAX_VALUE / 3) return;
-                val *= 3;
-            }
-
-            for (int i = 0; i < p5; i++) {
-                if (val > Integer.MAX_VALUE / 5) return;
-                val *= 5;
-            }
-
-            if (!seen.contains(val)) {
-                seen.add(val);
-                pq.add(val);
-            }
-
-            return;
-        }
-
-        if (idx == 1) {
-            for (int i = 0; i <= 30; i++) {
-                fun(p2 + i, p3, p5, 2);
-            }
-        } 
-        else if (idx == 2) {
-            for (int i = 0; i <= 20; i++) {
-                fun(p2, p3 + i, p5, 3);
-            }
-        } 
-        else {
-            for (int i = 0; i <= 14; i++) {
-                fun(p2, p3, p5 + i, 4);
-            }
-        }
-    }
-
     public int nthUglyNumber(int n) {
 
-        this.n = n;
-        pq = new PriorityQueue<>();
-        seen = new HashSet<>();
+        int[] ugly = new int[n];
+        ugly[0] = 1;
 
-        fun(0, 0, 0, 1);
+        int p2 = 0;
+        int p3 = 0;
+        int p5 = 0;
 
-        while (n > 1) {
-            pq.poll();
-            n--;
+        for (int i = 1; i < n; i++) {
+
+            int next2 = ugly[p2] * 2;
+            int next3 = ugly[p3] * 3;
+            int next5 = ugly[p5] * 5;
+
+            int nextUgly = Math.min(next2, Math.min(next3, next5));
+            ugly[i] = nextUgly;
+
+            if (nextUgly == next2) {
+                p2++;
+            }
+            if (nextUgly == next3) {
+                p3++;
+            }
+            if (nextUgly == next5) {
+                p5++;
+            }
         }
 
-        return pq.poll();
+        return ugly[n - 1];
     }
 }
