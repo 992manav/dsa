e8d521f37bf32969ea397ff80a573250374476a5
Manav Desai <dmanavpianist@gmail.com>
2026-01-23 18:33:56 +0530

    Time: 34 ms (24.02%) | Memory: 274 MB (20.67%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -3,32 +3,68 @@ class Solution {
     int[][] q;
     int[] p;
 
-    boolean ok(int k){
-        int n=a.length;
-        for(int i=0;i<=n;i++) p[i]=0;
-        for(int i=0;i<k;i++){
-            int[] x=q[i];
-            p[x[0]]+=x[2];
-            int e=x[1]+1;
-            if(e<n) p[e]-=x[2];
+    boolean ok(int k) {
+        int n = a.length;
+
+        for (int i = 0; i <= n; i++) {
+            p[i] = 0;
         }
-        for(int i=1;i<n;i++) p[i]+=p[i-1];
-        for(int i=0;i<n;i++) if(p[i]<a[i]) return false;
+
+        for (int i = 0; i < k; i++) {
+            int[] x = q[i];
+
+            int l = x[0];
+            int r = x[1];
+            int v = x[2];
+
+            p[l] += v;
+
+            int end = r + 1;
+            if (end < n) {
+                p[end] -= v;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            p[i] = p[i] + p[i - 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int have = p[i];
+            int need = a[i];
+
+            if (have < need) {
+                return false;
+            }
+        }
+
         return true;
     }
 
-    public int minZeroArray(int[] nums,int[][] queries){
-        a=nums;
-        q=queries;
-        p=new int[a.length+1];
-        int l=0,r=q.length,ans=-1;
-        while(l<=r){
-            int m=(l+r)>>1;
-            if(ok(m)){
-                ans=m;
-                r=m-1;
-            }else l=m+1;
+    public int minZeroArray(int[] nums, int[][] queries) {
+        a = nums;
+        q = queries;
+
+        int n = a.length;
+        p = new int[n + 1];
+
+        int l = 0;
+        int r = q.length;
+        int ans = -1;
+
+        while (l <= r) {
+            int m = (l + r) >> 1;
+
+            boolean possible = ok(m);
+
+            if (possible) {
+                ans = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
         }
+
         return ans;
     }
 }
