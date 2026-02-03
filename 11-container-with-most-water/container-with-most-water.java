639f83c067ca6955c604fee7c97755b883124305
Manav Desai <dmanavpianist@gmail.com>
2026-02-03 20:33:17 +0530

    Time: 198 ms (5.62%) | Memory: 78 MB (9.10%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,42 +1,79 @@
+import java.util.*;
+
 class Solution {
 
-    public int find_leftmost(int[] height, int idx) {
-        for (int i = 0; i < height.length; i++) {
-            if (height[i] >= height[idx]) {
-                return i;
-            }
-        }
-        return -1;
-    }
+    int binary_search(List<Integer> keys, int target) {
+        int low = 0;
+        int high = keys.size() - 1;
+        int ans = -1;
 
-    public int find_rightmost(int[] height, int idx) {
-        for (int i = height.length - 1; i >= 0; i--) {
-            if (height[i] >= height[idx]) {
-                return i;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (keys.get(mid) >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return -1;
+
+        return ans;
     }
 
     public int maxArea(int[] height) {
-        // har danda uske leftmost aur rightmost usse bada danda dhundhega 
-        int max_area = 0;
+        int n = height.length;
 
-        for (int i = 0; i < height.length; i++) {
-            int left = find_leftmost(height, i);
-            int right = find_rightmost(height, i);
+        TreeMap<Integer, int[]> map = new TreeMap<>();
 
-            if (left != -1) {  // avoid invalid area
-                int area = (i - left) * height[i];
-                max_area = Math.max(area, max_area);
-            }
-
-            if (right != -1) {  // avoid invalid area
-                int area = (right - i) * height[i];
-                max_area = Math.max(area, max_area);
+        for (int i = 0; i < n; i++) {
+            if (!map.containsKey(height[i])) {
+                map.put(height[i], new int[]{i, i});
+            } else {
+                map.get(height[i])[1] = i;
             }
         }
 
-        return max_area;
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        int m = keys.size();
+
+        int[] left = new int[m];
+        int[] right = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            left[i] = map.get(keys.get(i))[0];
+            right[i] = map.get(keys.get(i))[1];
+        }
+
+        int[] suffix_max = new int[m];
+        int[] prefix_min = new int[m];
+
+        suffix_max[m - 1] = right[m - 1];
+        prefix_min[m - 1] = left[m - 1];
+
+        for (int i = m - 2; i >= 0; i--) {
+            suffix_max[i] = Math.max(right[i], suffix_max[i + 1]);
+            prefix_min[i] = Math.min(left[i], prefix_min[i + 1]);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int idx = binary_search(keys, height[i]);
+            if (idx == -1) continue;
+
+            int ri = suffix_max[idx];
+            int li = prefix_min[idx];
+
+            if (ri != i) {
+                ans = Math.max(ans, height[i] * Math.abs(ri - i));
+            }
+
+            if (li != i) {
+                ans = Math.max(ans, height[i] * Math.abs(li - i));
+            }
+        }
+
+        return ans;
     }
 }
