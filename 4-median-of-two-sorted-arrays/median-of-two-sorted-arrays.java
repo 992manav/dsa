8e8854043065cb5ac556d7534805bedc1075a048
Manav Desai <dmanavpianist@gmail.com>
2026-02-04 12:47:14 +0530

    Time: 1 ms (99.45%) | Memory: 49 MB (28.47%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,69 +1,57 @@
 class Solution {
-
-    int check(int[] A, int[] B, int mid) {
-        int total_elements = (A.length + B.length) / 2;
-        int no_B = total_elements - mid;
-
-        int l1 = mid - 1;
-        int l2 = no_B - 1;
-        int r1 = mid;
-        int r2 = no_B;
-
-        int left1 = (l1 >= 0) ? A[l1] : Integer.MIN_VALUE;
-        int left2 = (l2 >= 0) ? B[l2] : Integer.MIN_VALUE;
-        int right1 = (r1 < A.length) ? A[r1] : Integer.MAX_VALUE;
-        int right2 = (r2 < B.length) ? B[r2] : Integer.MAX_VALUE;
-
-        if (left1 > right2) return -1;
-        if (left2 > right1) return 1;
-        return 0;
-    }
-
-    double calculateMedian(int[] A, int[] B, int mid, boolean isOdd) {
-        int total_elements = (A.length + B.length) / 2;
-        int no_B = total_elements - mid;
-
-        int l1 = mid - 1;
-        int l2 = no_B - 1;
-        int r1 = mid;
-        int r2 = no_B;
-
-        int left = Math.max((l1 >= 0) ? A[l1] : Integer.MIN_VALUE,
-                            (l2 >= 0) ? B[l2] : Integer.MIN_VALUE);
-        int right = Math.min((r1 < A.length) ? A[r1] : Integer.MAX_VALUE,
-                             (r2 < B.length) ? B[r2] : Integer.MAX_VALUE);
-
-        return isOdd ? right : (left + right) / 2.0;
-    }
-
-    double binarySearch(int[] A, int[] B, int low, int high, boolean isOdd) {
-        int ans = -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int res = check(A, B, mid);
-            if (res == 1) {
-                low = mid + 1;
-            } else if (res == -1) {
-                high = mid - 1;
-            } else {
-                ans = mid;
-                break;
-            }
-        }
-        return calculateMedian(A, B, ans, isOdd);
-    }
-
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        boolean isOdd = ((nums1.length + nums2.length) % 2 != 0);
-        int low = 0;
-        int high;
 
-        if (nums1.length < nums2.length) {
-            high = nums1.length;
-            return binarySearch(nums1, nums2, low, high, isOdd);
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int tot = n + m;
+        int count = tot / 2 + tot % 2;
+
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+
+        while (count > 0) {
+
+            if (i < n && j < m) {
+                if (nums1[i] > nums2[j]) {
+                    ans = nums2[j];
+                    j++;
+                } else {
+                    ans = nums1[i];
+                    i++;
+                }
+            } else {
+                if (i == n) {
+                    ans = nums2[j];
+                    j++;
+                } else {
+                    ans = nums1[i];
+                    i++;
+                }
+            }
+
+            count--;
+        }
+
+        if (tot % 2 == 0) {
+
+            if (i < n && j < m) {
+                if (nums1[i] > nums2[j]) {
+                    return (ans + nums2[j]) / 2.0;
+                } else {
+                    return (ans + nums1[i]) / 2.0;
+                }
+            } else {
+                if (i == n) {
+                    return (ans + nums2[j]) / 2.0;
+                } else {
+                    return (ans + nums1[i]) / 2.0;
+                }
+            }
+
         } else {
-            high = nums2.length;
-            return binarySearch(nums2, nums1, low, high, isOdd);
+            return ans;
         }
     }
 }
