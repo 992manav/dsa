b3f00e3c7d4dd87058f4513551fadac45cec277c
Manav Desai <dmanavpianist@gmail.com>
2026-02-12 18:36:53 +0530

    Time: 269 ms (29.35%) | Memory: 47.1 MB (7.22%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.java
+++ 686-repeated-string-match/repeated-string-match.java
@@ -1,25 +1,17 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        
-        while (sb.length() < b.length()) {
+        StringBuilder sb = new StringBuilder(a);
+        int count = 1;
+
+        while (!sb.toString().contains(b)) {
             sb.append(a);
             count++;
+
+            if (count > (b.length() / a.length()) + 2) {
+                return -1;
+            }
         }
-        
-        if (sb.toString().contains(b)) {
-            return count;
-        }
-        
-        sb.append(a);
-        count++;
-        
-        if (sb.toString().contains(b)) {
-            return count;
-        }
-        
-        return -1;
+
+        return count;
     }
 }
