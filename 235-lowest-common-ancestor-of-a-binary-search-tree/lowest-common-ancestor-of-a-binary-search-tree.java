c49358fe442f060c0d9eab630e58d2009ca8e45c
Manav Desai <dmanavpianist@gmail.com>
2026-02-15 22:55:11 +0530

    Time: 6 ms (97.05%) | Memory: 48.1 MB (14.75%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,60 +1,35 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
 class Solution {
+
     TreeNode p;
     TreeNode q;
+
     TreeNode fun(TreeNode node){
 
-        if(node==null){
+        if(node == null){
             return null;
         }
 
-        boolean flag=false;
-        if(node==p || node==q){
-            flag=true;
-        }
-
-        TreeNode l=fun(node.left);
-        TreeNode r=fun(node.right);
-
-        if(l!=null && r!=null){
+        if(node == p || node == q){
             return node;
         }
 
-        if(l!=null){
-            if(flag){
-                return node;
-            }
+        TreeNode l = fun(node.left);
+        TreeNode r = fun(node.right);
+
+        if(l != null && r != null){
+            return node;
+        }
+
+        if(l != null){
             return l;
         }
 
-
-        if(r!=null){
-            if(flag){
-                return node;
-            }
-            return r;
-        }
-
-
-        if(flag){
-                return node;
-        }
-        return null;
-
+        return r;
     }
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        this.p=p;
-        this.q=q;
-        TreeNode lca=fun(root);
-        return lca;
+        this.p = p;
+        this.q = q;
+        return fun(root);
     }
 }
