1403c929de6012e191d885090725ee9ab9674061
Manav Desai <dmanavpianist@gmail.com>
2026-02-15 16:10:57 +0530

    Time: 1423 ms (12.68%) | Memory: 223.3 MB (13.66%) - LeetSync

--- 1881-closest-subsequence-sum/closest-subsequence-sum.java
+++ 1881-closest-subsequence-sum/closest-subsequence-sum.java
@@ -7,17 +7,16 @@ class Solution {
     int goal;
     int min;
 
-    void fun(int i, int sum, int end, Map<Integer, Integer> map){
+    void fun(int i, int sum, int end, TreeSet<Integer> set){
 
         if(i == end){
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            set.add(sum);
             min = Math.min(Math.abs(goal - sum), min);
             return;
         }
 
-        
-        fun(i + 1, sum + nums[i], end, map);
-        fun(i + 1, sum, end, map);
+        fun(i + 1, sum + nums[i], end, set);
+        fun(i + 1, sum, end, set);
     }
 
     public int minAbsDifference(int[] nums, int goal) {
@@ -28,35 +27,44 @@ class Solution {
         n = nums.length;
         min = Integer.MAX_VALUE;
 
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        TreeMap<Integer,Integer> map1 = new TreeMap<>();
-        
-        Arrays.sort(nums);
+        int minPossible = 0;
+        int maxPossible = 0;
 
-        fun(0, 0, n/2, map);
-        fun(n/2, 0, n, map1);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) {
+                maxPossible += nums[i];
+            } else {
+                minPossible += nums[i];
+            }
+        }
 
-        for(Integer key : map.keySet()){
+        if (maxPossible < goal) {
+            return Math.abs(maxPossible - goal);
+        }
+        if (minPossible > goal) {
+            return Math.abs(minPossible - goal);
+        }
+
+        TreeSet<Integer> set1 = new TreeSet<>();
+        TreeSet<Integer> set2 = new TreeSet<>();
+
+        fun(0, 0, n/2, set1);
+        fun(n/2, 0, n, set2);
+
+        for(Integer key : set1){
 
             int target = goal - key;
 
-            // // smallest value in map1
-            // Integer first = map1.firstKey();
-            // min = Math.min(min, Math.abs(goal - (key + first)));
-
-            // closest lower or equal
-            Integer floor = map1.floorKey(target);
+            Integer floor = set2.floor(target);
             if(floor != null){
                 min = Math.min(min, Math.abs(goal - (key + floor)));
             }
 
-            // closest greater or equal
-            Integer ceil = map1.ceilingKey(target);
+            Integer ceil = set2.ceiling(target);
             if(ceil != null){
                 min = Math.min(min, Math.abs(goal - (key + ceil)));
             }
         }
-
 
         return min;
     }
