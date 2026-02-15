819477d6064986dab9fbdc363b2585b53a908d8b
Manav Desai <dmanavpianist@gmail.com>
2026-02-15 13:02:52 +0530

    Time: 6 ms (7.22%) | Memory: 44.4 MB (10.05%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,52 +1,10 @@
+import java.math.BigInteger;
+
 class Solution {
     public String addBinary(String a, String b) {
-
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        int carry = 0;
-
-        StringBuilder sb = new StringBuilder();
-
-        while (Math.max(i, j) >= 0) {
-            char c1;
-            if (i < 0) {
-                c1 = '0';
-            } else {
-                c1 = a.charAt(i);
-            }
-
-            char c2;
-            if (j < 0) {
-                c2 = '0';
-            } else {
-                c2 = b.charAt(j);
-            }
-
-            if ((c1 ^ c2) == 0) {
-                sb.append((char)(carry + '0'));
-                if (c1 == '0') {
-                    carry = 0;
-                } else {
-                    carry = 1;
-                }
-            } else {
-                if (carry == 1) {
-                    sb.append('0');
-                    carry = 1;
-                } else {
-                    sb.append('1');
-                    carry = 0;
-                }
-            }
-
-            i--;
-            j--;
-        }
-
-        if (carry == 1) {
-            sb.append('1');
-        }
-
-        return sb.reverse().toString();
+        BigInteger x = new BigInteger(a, 2);
+        BigInteger y = new BigInteger(b, 2);
+        BigInteger sum = x.add(y);
+        return sum.toString(2);
     }
 }
