ff9ca6d5526cd681bdec12c70b0a8ef305fa2f6f
Manav Desai <dmanavpianist@gmail.com>
2026-02-16 10:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (10.44%) - LeetSync

---
+++ 458-poor-pigs/poor-pigs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        
+        int rounds = minutesToTest / minutesToDie;
+        int states = rounds + 1;
+        
+        int pigs = 0;
+        int total = 1;
+        
+        while (total < buckets) {
+            pigs++;
+            total = total * states;
+        }
+        
+        return pigs;
+    }
+}
