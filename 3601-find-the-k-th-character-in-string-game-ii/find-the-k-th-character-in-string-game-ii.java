1948c0082d9bbf2d572b11aee06e8a06773d4e6e
Manav Desai <dmanavpianist@gmail.com>
2026-03-03 18:25:57 +0530

    Time: 1 ms (100.00%) | Memory: 45 MB (77.65%) - LeetSync

--- 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
+++ 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
@@ -1,23 +1,36 @@
 class Solution {
-    char fun(long k, int i, long shift, int[] ops) {
+
+    char fun(long n, long k, int i, long shift, int[] ops) {
         if (i < 0) {
-            return (char)('a' + (shift % 26));
+            return (char) ('a' + (shift % 26));
         }
 
-        // Use Math.min to cap half, since k < 2^63 anyway
-        long half = (i >= 63) ? Long.MAX_VALUE : (1L << i);
+        long half = n / 2;
 
         if (k < half) {
-            return fun(k, i - 1, shift, ops);
+            return fun(half, k, i - 1, shift, ops);
         } else {
             if (ops[i] == 1) {
                 shift++;
             }
-            return fun(k - half, i - 1, shift, ops);
+            return fun(half, k - half, i - 1, shift, ops);
         }
     }
 
     public char kthCharacter(long k, int[] ops) {
-        return fun(k - 1, ops.length - 1, 0L, ops);
+        int last_idx = ops.length - 1;
+
+        long n = 1L;
+
+        for (int i = 0; i < ops.length; i++) {
+            n = n * 2;
+
+            if (n > k) {
+                last_idx = i;
+                break;
+            }
+        }
+
+        return fun(n, k - 1, last_idx, 0L, ops);
     }
 }
