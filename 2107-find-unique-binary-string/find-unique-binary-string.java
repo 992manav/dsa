1b2883507dfb04f9ebf22bf3f0edc07a34524815
Manav Desai <dmanavpianist@gmail.com>
2026-03-09 21:51:15 +0530

    Time: 4 ms (19.40%) | Memory: 46.9 MB (9.41%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,25 +1,56 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        int n = nums.length;
 
+        int n = nums.length;
         int[] arr = new int[1 << n];
 
+        char[] or = nums[0].toCharArray();
+
         for(int i = 0; i < n; i++){
-            arr[Integer.parseInt(nums[i], 2)] = 1;
-        }
+            String s = nums[i];
 
-        for(int i = 0; i < (1 << n); i++){
-            if(arr[i] == 0){
-                String s = Integer.toBinaryString(i);
-
-                while(s.length() < n){
-                    s = "0" + s;
+            for(int j = 0; j < s.length(); j++){
+                if(s.charAt(j) == '1'){
+                    or[j] = '1';
                 }
-
-                return s;
             }
         }
 
-        return "";
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < or.length; i++){
+            if(or[i] == '1'){
+                sb.append('0');
+            }else{
+                sb.append('1');
+            }
+        }
+
+        String str = sb.toString();
+
+        for(int i = 0; i < n; i++){
+            if(nums[i].equals(str)){
+
+                for(int j = 0; j < n; j++){
+                    arr[Integer.parseInt(nums[j], 2)] = 1;
+                }
+
+                for(int j = 0; j < (1 << n); j++){
+                    if(arr[j] == 0){
+                        String s = Integer.toBinaryString(j);
+
+                        while(s.length() < n){
+                            s = "0" + s;
+                        }
+
+                        return s;
+                    }
+                }
+
+                return "";
+            }
+        }
+
+        return str;
     }
 }
