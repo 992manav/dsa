61d8d9c6d912c961a540290589ad5173069c94d0
Manav Desai <dmanavpianist@gmail.com>
2026-03-10 18:02:57 +0530

    Time: 359 ms (5.13%) | Memory: 96.8 MB (26.92%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
@@ -5,12 +5,14 @@ class Solution {
     int calc_hash(String s){
         int hash = 0;
         int j = 0;
+
         while(j < s.length()){
             char c = s.charAt(j);
             int idx = c - 'a' + 1;
             hash = hash * 26 + idx;
             j++;
         }
+
         return hash;
     }
 
@@ -25,28 +27,42 @@ class Solution {
 
         for(int i = 0; i < f.size(); i++){
             String s = f.get(i);
+
             int len = s.length();
 
-            if(len > max) max = len;
-            if(len < min) min = len;
+            if(len > max){
+                max = len;
+            }
+
+            if(len < min){
+                min = len;
+            }
 
             set.add(calc_hash(s));
         }
 
-        int[] max_start = new int[n];
-        Arrays.fill(max_start, -1);
+        int[] min_len = new int[n];
+        int[] max_len = new int[n];
+
+        Arrays.fill(min_len, n);
+        Arrays.fill(max_len, -1);
 
         for(int len = min; len <= max; len++){
 
-            if(len > n) break;
+            if(len > n){
+                break;
+            }
 
             int hash = 0;
             int p = 1;
 
             for(int k = 0; k < len; k++){
+
                 char c = word.charAt(k);
                 int idx = c - 'a' + 1;
+
                 hash = hash * 26 + idx;
+
                 if(k < len - 1){
                     p = p * 26;
                 }
@@ -56,28 +72,33 @@ class Solution {
             int j = len - 1;
 
             if(set.contains(hash)){
-                max_start[j] = Math.max(max_start[j], i);
+                min_len[i] = Math.min(min_len[i], j);
+                max_len[i] = Math.max(max_len[i], j);
             }
 
             while(j + 1 < n){
+
                 hash = hash - (word.charAt(i) - 'a' + 1) * p;
                 hash = hash * 26 + (word.charAt(j + 1) - 'a' + 1);
+
                 i++;
                 j++;
+
                 if(set.contains(hash)){
-                    max_start[j] = Math.max(max_start[j], i);
+                    min_len[i] = Math.min(min_len[i], j);
+                    max_len[i] = Math.max(max_len[i], j);
                 }
             }
         }
 
         int ans = 0;
-        int left = 0;
+        int right = n;
 
-        for(int r = 0; r < n; r++){
-            if(max_start[r] != -1){
-                left = Math.max(left, max_start[r] + 1);
-            }
-            ans = Math.max(ans, r - left + 1);
+        for(int i = n - 1; i >= 0; i--){
+
+            right = Math.min(right, min_len[i]);
+
+            ans = Math.max(ans, right - i);
         }
 
         return ans;
