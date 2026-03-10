7eee0700a240d216a62d45da34398fb41c11e8bc
Manav Desai <dmanavpianist@gmail.com>
2026-03-10 18:04:13 +0530

    Time: 324 ms (6.41%) | Memory: 95 MB (26.92%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
@@ -5,9 +5,10 @@ class Solution {
     int calc_hash(String s){
         int hash = 0;
         int j = 0;
-        while(j < s.length()){
-            char c = s.charAt(j);
-            int idx = c - 'a' + 1;
+        int len = s.length();
+
+        while(j < len){
+            int idx = s.charAt(j) - 'a' + 1;
             hash = hash * 26 + idx;
             j++;
         }
@@ -17,6 +18,7 @@ class Solution {
     public int longestValidSubstring(String word, List<String> f) {
 
         int n = word.length();
+        char[] arr = word.toCharArray();
 
         int min = 11;
         int max = 0;
@@ -26,44 +28,55 @@ class Solution {
         for(int i = 0; i < f.size(); i++){
             String s = f.get(i);
             int len = s.length();
+
             if(len > max) max = len;
             if(len < min) min = len;
+
             set.add(calc_hash(s));
         }
 
         int[] max_start = new int[n];
         Arrays.fill(max_start, -1);
 
+        int[] pow = new int[max + 1];
+        pow[0] = 1;
+
+        for(int i = 1; i <= max; i++){
+            pow[i] = pow[i - 1] * 26;
+        }
+
         for(int len = min; len <= max; len++){
 
             if(len > n) break;
 
             int hash = 0;
-            int p = 1;
 
             for(int k = 0; k < len; k++){
-                char c = word.charAt(k);
-                int idx = c - 'a' + 1;
+                int idx = arr[k] - 'a' + 1;
                 hash = hash * 26 + idx;
-                if(k < len - 1){
-                    p = p * 26;
-                }
             }
 
             int i = 0;
             int j = len - 1;
 
             if(set.contains(hash)){
-                max_start[j] = Math.max(max_start[j], i);
+                if(max_start[j] < i){
+                    max_start[j] = i;
+                }
             }
 
             while(j + 1 < n){
-                hash = hash - (word.charAt(i) - 'a' + 1) * p;
-                hash = hash * 26 + (word.charAt(j + 1) - 'a' + 1);
+
+                hash = hash - (arr[i] - 'a' + 1) * pow[len - 1];
+                hash = hash * 26 + (arr[j + 1] - 'a' + 1);
+
                 i++;
                 j++;
+
                 if(set.contains(hash)){
-                    max_start[j] = Math.max(max_start[j], i);
+                    if(max_start[j] < i){
+                        max_start[j] = i;
+                    }
                 }
             }
         }
@@ -72,10 +85,18 @@ class Solution {
         int left = 0;
 
         for(int r = 0; r < n; r++){
+
             if(max_start[r] != -1){
-                left = Math.max(left, max_start[r] + 1);
+                int val = max_start[r] + 1;
+                if(val > left){
+                    left = val;
+                }
             }
-            ans = Math.max(ans, r - left + 1);
+
+            int len = r - left + 1;
+            if(len > ans){
+                ans = len;
+            }
         }
 
         return ans;
