9ca07847de49950df980913a55b86c683e4e6842
Manav Desai <dmanavpianist@gmail.com>
2026-03-18 16:24:50 +0530

    Time: 69 ms (5.33%) | Memory: 68.4 MB (6.56%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,39 +1,57 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l = 0;
-        int r = 0;
-        int len = 0;
-        int max_count = 0;
-        int max_length = 0;
-        
-        int[] freq = new int[26];
+        int n = s.length();
 
-        while (r < s.length()) {
-            char c = s.charAt(r);
-            freq[c - 'A']++;
-            len++;
+        int[][] cnt = new int[26][n];
 
-            if (freq[c - 'A'] > max_count) {
-                max_count = freq[c - 'A'];
-            }
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            for (int i = 0; i < n; i++) {
 
-            while (len - max_count > k) {
-                freq[s.charAt(l) - 'A']--;
-                l++;
-                len--;
-                max_count = 0;
-                for (int i = 0; i < 26; i++) {
-                    if (freq[i] > max_count) {
-                        max_count = freq[i];
+                if (i == 0) {
+                    if (s.charAt(i) == ch) {
+                        cnt[ch - 'A'][i] = 1;
                     }
+                    continue;
+                }
+
+                if (s.charAt(i) == ch) {
+                    cnt[ch - 'A'][i] = cnt[ch - 'A'][i - 1] + 1;
+                } else {
+                    cnt[ch - 'A'][i] = cnt[ch - 'A'][i - 1];
                 }
             }
-
-            if (len > max_length) {
-                max_length = len;
-            }
-            r++;
         }
-        return max_length;
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int low = i;
+            int high = n - 1;
+
+            while (low <= high) {
+
+                int mid = low + (high - low) / 2;
+
+                int noOfEle = cnt[s.charAt(i) - 'A'][mid];
+
+                if (i != 0) {
+                    noOfEle -= cnt[s.charAt(i) - 'A'][i - 1];
+                }
+
+                int len = mid - i + 1;
+                int extra_needed = len - noOfEle;
+
+                if (extra_needed <= k) {
+                    int temp = k - extra_needed;
+                    ans = Math.max(ans, Math.min(n, temp + len));
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return ans;
     }
 }
